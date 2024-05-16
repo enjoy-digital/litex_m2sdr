33d94c14c46dd9db20698b229bf50a8476589d9f
Florent Kermarrec <florent@enjoy-digital.fr>
2024-05-16 18:34:16 +0200

    Add pcie_radio platform/target to test project on validated hardware.

---
+++ pcie_radio.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-M2SDR.
+#
+# Copyright (c) 2024 Enjoy-Digital <enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import os
+import argparse
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+from migen.genlib.cdc import PulseSynchronizer, MultiReg
+
+from litex.gen import *
+
+from pcie_radio_platform import Platform
+
+from litex.soc.interconnect.csr import *
+from litex.soc.interconnect     import stream
+
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder  import *
+
+from litex.soc.cores.clock     import *
+from litex.soc.cores.led       import LedChaser
+from litex.soc.cores.icap      import ICAP
+from litex.soc.cores.xadc      import XADC
+from litex.soc.cores.dna       import DNA
+from litex.soc.cores.pwm       import PWM
+from litex.soc.cores.bitbang   import I2CMaster
+from litex.soc.cores.spi       import SPIMaster
+
+from litex.build.generic_platform import IOStandard, Subsignal, Pins
+
+from litepcie.phy.s7pciephy import S7PCIEPHY
+
+from liteeth.phy.a7_gtp import QPLLSettings, QPLL
+from liteeth.phy.a7_1000basex import A7_1000BASEX
+
+from litescope import LiteScopeAnalyzer
+
+from software import generate_litepcie_software
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class CRG(LiteXModule):
+    def __init__(self, platform, sys_clk_freq):
+        self.cd_sys    = ClockDomain()
+        self.cd_idelay = ClockDomain()
+
+        # # #
+
+        # Clk / Rst.
+        clk38p4 = platform.request("clk38p4")
+
+        # PLL.
+        self.pll = pll = S7PLL(speedgrade=-2)
+        pll.register_clkin(clk38p4, 38.4e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
+        pll.create_clkout(self.cd_idelay, 200e6)
+
+        # IDelayCtrl.
+        self.idelayctrl = S7IDELAYCTRL(self.cd_idelay)
+
+# BaseSoC -----------------------------------------------------------------------------------------
+
+class BaseSoC(SoCMini):
+    def __init__(self, sys_clk_freq=int(125e6), with_pcie=True, pcie_lanes=1, with_jtagbone=True):
+        # Platform ---------------------------------------------------------------------------------
+        platform = Platform()
+
+        # SoCMini ----------------------------------------------------------------------------------
+        SoCMini.__init__(self, platform, sys_clk_freq,
+            ident         = f"LiteX SoC on PCIe-Radio",
+            ident_version = True,
+        )
+
+        # Clocking ---------------------------------------------------------------------------------
+        self.crg = CRG(platform, sys_clk_freq)
+        platform.add_platform_command("set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets basesoc_crg_clkin]")
+
+        # SI5351 Clock Generator -------------------------------------------------------------------
+        class SI5351FakePads:
+            def __init__(self):
+                self.scl = Signal()
+                self.sda = Signal()
+                self.pwm = Signal()
+        si5351_fake_pads = SI5351FakePads()
+
+        self.si5351_i2c = I2CMaster(pads=si5351_fake_pads)
+        self.si5351_pwm = PWM(si5351_fake_pads.pwm,
+            default_enable = 1,
+            default_width  = 1024,
+            default_period = 2048,
+        )
+
+        # JTAGBone ---------------------------------------------------------------------------------
+        if with_jtagbone:
+            self.add_jtagbone()
+            platform.add_period_constraint(self.jtagbone_phy.cd_jtag.clk, 1e9/20e6)
+            platform.add_false_path_constraints(self.jtagbone_phy.cd_jtag.clk, self.crg.cd_sys.clk)
+
+        # Leds -------------------------------------------------------------------------------------
+        self.leds = LedChaser(
+            pads         = platform.request_all("user_led"),
+            sys_clk_freq = sys_clk_freq
+        )
+
+        # ICAP -------------------------------------------------------------------------------------
+        self.icap = ICAP()
+        self.icap.add_reload()
+        self.icap.add_timing_constraints(platform, sys_clk_freq, self.crg.cd_sys.clk)
+
+        # XADC -------------------------------------------------------------------------------------
+        self.xadc = XADC()
+
+        # DNA --------------------------------------------------------------------------------------
+        self.dna = DNA()
+        self.dna.add_timing_constraints(platform, sys_clk_freq, self.crg.cd_sys.clk)
+
+        # PCIe -------------------------------------------------------------------------------------
+        if with_pcie:
+            self.pcie_phy = S7PCIEPHY(platform, platform.request(f"pcie_x{pcie_lanes}"),
+                data_width  = {1: 64, 4: 128}[pcie_lanes],
+                bar0_size   = 0x20000,
+                cd          = "sys",
+            )
+            if pcie_lanes == 1:
+                platform.toolchain.pre_placement_commands.append("reset_property LOC [get_cells -hierarchical -filter {{NAME=~pcie_s7/*gtp_channel.gtpe2_channel_i}}]")
+                platform.toolchain.pre_placement_commands.append("set_property LOC GTPE2_CHANNEL_X0Y2 [get_cells -hierarchical -filter {{NAME=~pcie_s7/*gtp_channel.gtpe2_channel_i}}]")
+            self.pcie_phy.update_config({
+                "Base_Class_Menu"          : "Wireless_controller",
+                "Sub_Class_Interface_Menu" : "RF_controller",
+                "Class_Code_Base"          : "0D",
+                "Class_Code_Sub"           : "10",
+                }
+            )
+            self.add_pcie(phy=self.pcie_phy, address_width=32, ndmas=1,
+                with_dma_buffering    = True, dma_buffering_depth=8192,
+                with_dma_loopback     = True,
+                with_dma_synchronizer = True,
+                with_msi              = True
+            )
+
+            # Timing Constraints/False Paths -------------------------------------------------------
+            for i in range(4):
+                platform.toolchain.pre_placement_commands.append(f"set_clock_groups -group [get_clocks {{{{*s7pciephy_clkout{i}}}}}] -group [get_clocks        dna_clk] -asynchronous")
+                platform.toolchain.pre_placement_commands.append(f"set_clock_groups -group [get_clocks {{{{*s7pciephy_clkout{i}}}}}] -group [get_clocks       jtag_clk] -asynchronous")
+                platform.toolchain.pre_placement_commands.append(f"set_clock_groups -group [get_clocks {{{{*s7pciephy_clkout{i}}}}}] -group [get_clocks       icap_clk] -asynchronous")
+
+        # AD9361 RFIC ------------------------------------------------------------------------------
+
+        ad9361_rfic_pads = platform.request("ad9361_rfic")
+
+        self.ad9361_enable = CSRStorage()
+        self.comb += [
+            ad9361_rfic_pads.rst_n.eq(self.ad9361_enable.storage),
+            ad9361_rfic_pads.enable.eq(self.ad9361_enable.storage),
+        ]
+
+        self.ad9361_spi = SPIMaster(
+            pads         = platform.request("ad9361_spi"),
+            data_width   = 32,
+            sys_clk_freq = sys_clk_freq,
+            spi_clk_freq = 1e6
+        )
+        self.ad9361_spi.add_clk_divider()
+
+        # Debug.
+        analyzer_signals = [platform.lookup_request("ad9361_spi")]
+        self.analyzer = LiteScopeAnalyzer(analyzer_signals,
+            depth        = 4096,
+            clock_domain = "sys",
+            register     = True,
+            csr_csv      = "analyzer.csv"
+        )
+
+        # Clk Measurements -------------------------------------------------------------------------
+
+        class ClkMeasurement(LiteXModule):
+            def __init__(self, clk, increment=1):
+                self.latch = CSR()
+                self.value = CSRStatus(64)
+
+                # # #
+
+                # Create Clock Domain.
+                self.cd_counter = ClockDomain()
+                self.comb += self.cd_counter.clk.eq(clk)
+                self.specials += AsyncResetSynchronizer(self.cd_counter, ResetSignal())
+
+                # Free-running Clock Counter.
+                counter = Signal(64)
+                self.sync.counter += counter.eq(counter + increment)
+
+                # Latch Clock Counter.
+                latch_value = Signal(64)
+                latch_sync  = PulseSynchronizer("sys", "counter")
+                self.submodules += latch_sync
+                self.comb += latch_sync.i.eq(self.latch.re)
+                self.sync.counter += If(latch_sync.o, latch_value.eq(counter))
+                self.specials += MultiReg(latch_value, self.value.status)
+
+        self.si5351_clk0_measurement = ClkMeasurement(clk=Signal())
+        self.si5351_clk1_measurement = ClkMeasurement(clk=Signal())
+        self.si5351_clk2_measurement = ClkMeasurement(clk=Signal())
+        self.si5351_clk3_measurement = ClkMeasurement(clk=Signal())
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="LiteX SoC on PCIe-Radio.")
+    parser.add_argument("--build",  action="store_true", help="Build bitstream.")
+    parser.add_argument("--load",   action="store_true", help="Load bitstream.")
+    parser.add_argument("--flash",  action="store_true", help="Flash bitstream.")
+    parser.add_argument("--driver", action="store_true", help="Generate PCIe driver from LitePCIe (override local version).")
+    comopts = parser.add_mutually_exclusive_group()
+    comopts.add_argument("--with-pcie",      action="store_true", help="Enable PCIe Communication.")
+    args = parser.parse_args()
+
+    # Build SoC.
+    soc = BaseSoC(
+        with_pcie     = args.with_pcie,
+    )
+    builder = Builder(soc, csr_csv="csr.csv")
+    builder.build(run=args.build)
+
+    # Generate LitePCIe Driver.
+    generate_litepcie_software(soc, "software", use_litepcie_software=args.driver)
+
+    # Load Bistream.
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".bit"))
+
+    # Flash Bitstream.
+    if args.flash:
+        prog = soc.platform.create_programmer()
+        prog.flash(0, os.path.join(builder.gateware_dir, soc.build_name + ".bin"))
+
+if __name__ == "__main__":
+    main()
---
+++ pcie_radio_platform.py
@@ -0,0 +1,91 @@
+#
+# This file is part of LiteX-M2SDR project.
+#
+# Copyright (c) 2024 Enjoy-Digital <enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+from litex.build.generic_platform import *
+from litex.build.xilinx import Xilinx7SeriesPlatform
+from litex.build.openfpgaloader import OpenFPGALoader
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+    # Clk/Rst.
+    ("clk38p4", 0, Pins("V22"), IOStandard("LVCMOS33")),
+
+    # Leds.
+    ("user_led", 0, Pins("D22"),  IOStandard("LVCMOS33")),
+
+    # PCIe.
+    ("pcie_x1", 0,
+        Subsignal("rst_n", Pins("A13"), IOStandard("LVCMOS33")),
+        Subsignal("clk_p", Pins("F6")),
+        Subsignal("clk_n", Pins("E6")),
+        Subsignal("rx_p",  Pins("B10")),
+        Subsignal("rx_n",  Pins("A10")),
+        Subsignal("tx_p",  Pins("B6")),
+        Subsignal("tx_n",  Pins("A6"))
+    ),
+
+    # AD9361.
+    ("ad9361_rfic", 0,
+        #Subsignal("rx_clk_p",   Pins("L19"), IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("rx_clk_n",   Pins("L20"), IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("rx_frame_p", Pins("M15"), IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("rx_frame_n", Pins("M16"), IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("rx_data_p",  Pins("N20 K21 K17 J19 G17 N22"), IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("rx_data_n",  Pins("M20 K22 J17 H19 G18 M22"), IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+
+        #Subsignal("tx_clk_p",   Pins("N18"), IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("tx_clk_n",   Pins("N19"), IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("tx_frame_p", Pins("W9"),  IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("tx_frame_n", Pins("Y9"),  IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("tx_data_p",  Pins("AA5 Y4 AB7 AA1 W1 Y8"),  IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+        #Subsignal("tx_data_n",  Pins("AB5 AA4 AB6 AB1 Y1 Y7"), IOStandard("LVDS_25"), Misc("DIFF_TERM=TRUE")),
+
+        Subsignal("rst_n",  Pins("L18"), IOStandard("LVCMOS25")),
+        Subsignal("enable", Pins("W6"),  IOStandard("LVCMOS25")),
+        #Subsignal("txnrx",  Pins("AB8"), IOStandard("LVCMOS25")),
+        #Subsignal("en_agc", Pins("W5"),  IOStandard("LVCMOS25")),
+
+        #Subsignal("ctrl", Pins("U1 V2 U2 T1"), IOStandard("LVCMOS25")),
+        #Subsignal("stat", Pins("H13 H14 J14 G13 J16 H15 J15 G15"), IOStandard("LVCMOS25")),
+    ),
+    ("ad9361_spi", 0,
+        Subsignal("clk",  Pins("L14")),
+        Subsignal("cs_n", Pins("M17")),
+        Subsignal("mosi", Pins("L15")),
+        Subsignal("miso", Pins("L16")),
+        IOStandard("LVCMOS25")
+    ),
+]
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(Xilinx7SeriesPlatform):
+    default_clk_name   = "clk38p4"
+    default_clk_period = 1e9/38.4e6
+
+    def __init__(self, toolchain="vivado"):
+        Xilinx7SeriesPlatform.__init__(self, "xc7a35t-fgg484-2", _io, toolchain=toolchain)
+
+        self.toolchain.bitstream_commands = [
+            "set_property BITSTREAM.CONFIG.UNUSEDPIN Pulldown [current_design]",
+            "set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]",
+            "set_property BITSTREAM.CONFIG.CONFIGRATE 16 [current_design]",
+            "set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]",
+            "set_property CFGBVS VCCO [current_design]",
+            "set_property CONFIG_VOLTAGE 3.3 [current_design]",
+        ]
+        self.toolchain.additional_commands = [
+            "write_cfgmem -force -format bin -interface spix4 -size 16 -loadbit \"up 0x0 \
+            {build_name}.bit\" -file {build_name}.bin",
+        ]
+
+    def create_programmer(self):
+        return OpenFPGALoader(cable="digilent_hs2", fpga_part=f"xc7a35tfgg484", freq=10e6)
+
+    def do_finalize(self, fragment):
+        Xilinx7SeriesPlatform.do_finalize(self, fragment)
+        self.add_period_constraint(self.lookup_request("clk38p4", loose=True), 1e9/38.4e6)
