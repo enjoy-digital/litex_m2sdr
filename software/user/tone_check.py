c6f7f3bd5a720b1231a876b2e710ac4f06817ffb
Florent Kermarrec <florent@enjoy-digital.fr>
2024-05-17 22:30:22 +0200

    software/user: Add tone_gen/check scripts.

---
+++ software/user/tone_check.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import argparse
+import numpy as np
+from math import pi, cos, sin
+
+# Helpers ------------------------------------------------------------------------------------------
+
+def two_complement_decode(value, bits):
+    if value >= (1 << (bits - 1)):
+        value -= (1 << (bits))
+    return value
+
+def extract_header_timestamp(f):
+    header    = int.from_bytes(f.read(8), "little")
+    timestamp = int.from_bytes(f.read(8), "little")
+    return header, timestamp
+
+def calculate_rms(samples):
+    return np.sqrt(np.mean(np.square(samples)))
+
+# Tone Check ---------------------------------------------------------------------------------------
+
+def tone_check(filename, nchannels, nbits, samplerate, frame_header, frame_size, plot):
+    assert frame_size%8 == 0 # 64-bit
+    # Extract samples from file.
+    f = open(filename, "rb")
+    re = [[] for _ in range(nchannels)]
+    im = [[] for _ in range(nchannels)]
+    i = 0
+    while True:
+        done = False
+        if frame_header and ((8*i)%frame_size == 0):
+            header, timestamp = extract_header_timestamp(f)
+        for j in range(nchannels):
+            _bytes = f.read(4)
+            if _bytes == b"":
+                done = True
+                break
+            _re = int.from_bytes(_bytes[0:2], "little")
+            _im = int.from_bytes(_bytes[2:4], "little")
+            re[j].append(two_complement_decode(_re, nbits))
+            im[j].append(two_complement_decode(_im, nbits))
+        i += 1
+        if done:
+            break
+    f.close()
+
+    # Calculate and print RMS values
+    for j in range(nchannels):
+        rms_re = calculate_rms(re[j])
+        rms_im = calculate_rms(im[j])
+        print(f"RMS of Re{j}: {rms_re}")
+        print(f"RMS of Im{j}: {rms_im}")
+
+    # Plot Channel samples.
+    if plot:
+        import matplotlib.pyplot as plt
+        for j in range(nchannels):
+            plt.plot(re[j])
+            plt.plot(im[j])
+        plt.show()
+
+# Run ----------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="Tone Checker utility.")
+    parser.add_argument("filename", help="Ouptut filename.")
+    parser.add_argument("--nchannels",    type=int,   default=2,               help="Number of RF channels.")
+    parser.add_argument("--nbits",        type=int,   default=16,              help="Number of bits per Sample resolution (in bits per I/Q).")
+    parser.add_argument("--samplerate",   type=float, default=122.88e6,        help="Sample Rate.")
+    parser.add_argument("--frame-header", action="store_true",                 help="Extract Frame Header.")
+    parser.add_argument("--frame-size",   type=int,   default=122.88e6*8*1e-3, help="Frame Size default 1ms (Used when Frame Header enabled).")
+    parser.add_argument("--plot",         action= "store_true",                help="Enable Plot.")
+    args = parser.parse_args()
+
+    tone_check(
+        filename     = args.filename,
+        nchannels    = args.nchannels,
+        nbits        = args.nbits,
+        samplerate   = args.samplerate,
+        frame_header = args.frame_header,
+        frame_size   = args.frame_size,
+        plot         = args.plot,
+    )
+
+if __name__ == "__main__":
+    main()
---
+++ software/user/tone_gen.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from math import pi, cos, sin
+
+# Helpers ------------------------------------------------------------------------------------------
+
+def two_complement_encode(value, bits):
+    if (value & (1 << (bits - 1))) != 0:
+        value = value - (1 << bits)
+    return value % (2**bits)
+
+def insert_header_timestamp(f, header, timestamp):
+    f.write(   header.to_bytes(8, byteorder="little"))
+    f.write(timestamp.to_bytes(8, byteorder="little"))
+
+# Tone Gen -----------------------------------------------------------------------------------------
+
+def tone_gen(filename, nchannels, nbits, frequency, amplitude, samplerate, nsamples, frame_header, frame_size):
+    assert amplitude >= 0.0
+    assert amplitude <= 1.0
+    assert frame_size%8 == 0 # 64-bit
+    f = open(filename, "wb")
+    omega     = 2*pi*frequency/samplerate
+    phi       = 0
+    header    = 0x5aa5_5aa5_5aa5_5aa5
+    timestamp = 0
+    for i in range(int(nsamples)):
+        if frame_header and ((8*i)%frame_size == 0):
+            insert_header_timestamp(f, header, timestamp)
+        re = cos(phi) * amplitude * (2**(nbits - 1))
+        im = sin(phi) * amplitude * (2**(nbits - 1))
+        re = two_complement_encode(int(re), nbits)
+        im = two_complement_encode(int(im), nbits)
+        for j in range(nchannels):
+            f.write(re.to_bytes(2, byteorder="little"))
+            f.write(im.to_bytes(2, byteorder="little"))
+        phi += omega
+        if (phi >= pi):
+            phi -= 2*pi
+        timestamp += 1
+    f.close()
+
+# Run ----------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="Tone Generator utility.")
+    parser.add_argument("filename", help="Ouptut filename.")
+    parser.add_argument("--nchannels",    type=int,   default=2,               help="Number of RF channels.")
+    parser.add_argument("--nbits",        type=int,   default=16,              help="Number of bits per Sample resolution (in bits per I/Q).")
+    parser.add_argument("--frequency",    type=float, default=1e6,             help="Tone frequency.")
+    parser.add_argument("--amplitude",    type=float, default=1,               help="Tone amplitude.")
+    parser.add_argument("--samplerate",   type=float, default=122.88e6,        help="Sample Rate.")
+    parser.add_argument("--nsamples",     type=float, default=1e3,             help="Number of samples.")
+    parser.add_argument("--frame-header", action="store_true",                 help="Inserter Frame Header.")
+    parser.add_argument("--frame-size",   type=int,   default=122.88e6*8*1e-3, help="Frame Size (Used when Frame Header enabled).")
+    args = parser.parse_args()
+
+    tone_gen(
+        filename     = args.filename,
+        nchannels    = args.nchannels,
+        nbits        = args.nbits,
+        frequency    = args.frequency,
+        amplitude    = args.amplitude,
+        samplerate   = args.samplerate,
+        nsamples     = int(args.nsamples),
+        frame_header = args.frame_header,
+        frame_size   = args.frame_size,
+    )
+
+if __name__ == "__main__":
+    main()
