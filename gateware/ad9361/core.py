0bc32dd696d6a25acf34c633755fbfb2cece660b
Florent Kermarrec <florent@enjoy-digital.fr>
2024-05-22 11:48:13 +0200

    gateware/ad9361/core: Move AD9361 PRBS Generator/Checker to top.

--- gateware/ad9361/core.py
+++ gateware/ad9361/core.py
@@ -17,6 +17,58 @@ from litepcie.common import *
 from gateware.ad9361.phy import RFICPHY, phy_layout
 from gateware.ad9361.spi import SPIMaster
 
+# AD9361 PRBS Generator ----------------------------------------------------------------------------
+
+class AD9361PRBSGenerator(LiteXModule):
+    def __init__(self, seed=0x0a54):
+        self.ce = Signal(reset=1)
+        self.o  = Signal(16)
+
+        # # #
+
+        data = Signal(16, reset=seed)
+        self.sync += If(self.ce, data.eq(Cat((
+            data[1]  ^ data[2]  ^ data[4]  ^ data[5]  ^
+            data[6]  ^ data[7]  ^ data[8]  ^ data[9]  ^
+            data[10] ^ data[11] ^ data[12] ^ data[13] ^
+            data[14] ^ data[15]),
+            data[:-1]
+            )
+        ))
+        self.comb += self.o.eq(data)
+
+# AD9361 PRBS Checker ----------------------------------------------------------------------------
+
+class AD9361PRBSChecker(LiteXModule):
+    def __init__(self, seed=0x0a54):
+        self.ce     = Signal(reset=1)
+        self.i      = Signal(12)
+        self.synced = Signal()
+
+        # # #
+
+        error = Signal()
+
+        # # #
+
+        # PRBS reference
+        prbs = AD9361PRBSGenerator(seed=seed)
+        prbs = ResetInserter()(prbs)
+        self.submodules += prbs
+
+        # PRBS re-synchronization.
+        self.comb += prbs.ce.eq(self.ce)
+        self.comb += prbs.reset.eq(error)
+
+        # Error generation.
+        self.comb += If(self.ce, error.eq(self.i != prbs.o[:12]))
+
+
+        # Sync generation.
+        self.sync_timer = WaitTimer(1024)
+        self.comb += self.sync_timer.wait.eq(~error)
+        self.comb += self.synced.eq(self.sync_timer.done)
+
 # AD9361 RFIC --------------------------------------------------------------------------------------
 
 class AD9361RFIC(LiteXModule):
@@ -121,55 +173,6 @@ class AD9361RFIC(LiteXModule):
         # # #
 
         phy = self.phy
-
-        # AD9361 PRBS Generator/Checker classes.
-        class AD9361PRBSGenerator(LiteXModule):
-            def __init__(self, seed=0x0a54):
-                self.ce = Signal(reset=1)
-                self.o  = Signal(16)
-
-                # # #
-
-                data = Signal(16, reset=seed)
-                self.sync += If(self.ce, data.eq(Cat(
-                    (data[1]  ^ data[2]  ^ data[4]  ^ data[5]  ^
-                     data[6]  ^ data[7]  ^ data[8]  ^ data[9]  ^
-                     data[10] ^ data[11] ^ data[12] ^ data[13] ^
-                     data[14] ^ data[15]),
-                    data[:-1])
-                ))
-                self.comb += self.o.eq(data)
-
-
-        class AD9361PRBSChecker(LiteXModule):
-            def __init__(self, seed=0x0a54):
-                self.ce     = Signal(reset=1)
-                self.i      = Signal(12)
-                self.synced = Signal()
-
-                # # #
-
-                error = Signal()
-
-                # # #
-
-                # PRBS reference
-                prbs = AD9361PRBSGenerator(seed=seed)
-                prbs = ResetInserter()(prbs)
-                self.submodules += prbs
-
-                # PRBS re-synchronization.
-                self.comb += prbs.ce.eq(self.ce)
-                self.comb += prbs.reset.eq(error)
-
-                # Error generation.
-                self.comb += If(self.ce, error.eq(self.i != prbs.o[:12]))
-
-
-                # Sync generation.
-                self.sync_timer = WaitTimer(1024)
-                self.comb += self.sync_timer.wait.eq(~error)
-                self.comb += self.synced.eq(self.sync_timer.done)
 
         # PRBS TX.
         prbs_generator = AD9361PRBSGenerator()
