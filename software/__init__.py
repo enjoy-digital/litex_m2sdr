542cc5544e0d5667f31cef089e767b891473c9f5
Florent Kermarrec <florent@enjoy-digital.fr>
2024-05-28 08:56:07 +0200

    software: Add utilities to remove/rescan PCIe devices.

--- software/__init__.py
+++ software/__init__.py
@@ -1,10 +1,28 @@
 import os
+import subprocess
 
 from litex.build import tools
 
 from litex.soc.integration.export import get_csr_header, get_soc_header, get_mem_header
 
 from litepcie.software import copy_litepcie_software
+
+
+# Generic PCIe Utilities ---------------------------------------------------------------------------
+
+def get_pcie_device_id(vendor, device):
+    lspci_output = subprocess.check_output(["lspci", "-d", f"{vendor}:{device}"]).decode()
+    device_id = lspci_output.split()[0]
+    return device_id
+
+def remove_pcie_device(device_id, driver="litepcie"):
+    subprocess.run(["sudo", "rmmod", driver])
+    subprocess.run(["echo", "1", "|", "sudo", "tee", f"/sys/bus/pci/devices/0000:{device_id}/remove"])
+
+def rescan_pcie_bus():
+    subprocess.run(["echo", "1", "|", "sudo", "tee", "/sys/bus/pci/rescan"])
+
+# LitePCIe Software Generation ---------------------------------------------------------------------
 
 def generate_litepcie_software_headers(soc, dst):
     csr_header = get_csr_header(soc.csr_regions, soc.constants, with_csr_base_define=False, with_access_functions=False)
