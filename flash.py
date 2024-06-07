539bce40c507857f9e1912ded799df08e18aa46c
Florent Kermarrec <florent@enjoy-digital.fr>
2024-06-07 11:02:31 +0200

    flash.py: Cleanup/Improve.

--- flash.py
+++ flash.py
@@ -2,42 +2,61 @@
 
 import os
 import time
-import subprocess
 import argparse
+import subprocess
 
 from software import get_pcie_device_id, remove_pcie_device, rescan_pcie_bus
 
-def main():
-    parser = argparse.ArgumentParser(description="Flash FPGA bistream over PCIe")
-    parser.add_argument('bitstream', help='Path to the bitstream file')
-    parser.add_argument('-o', '--offset', type=lambda x: int(x, 0), default=0x00000000, help='Offset for flashing (default: 0x00000000)')
-    args = parser.parse_args()
+# Flash Utilities ----------------------------------------------------------------------------------
 
+def flash_bitstream(bitstream, offset):
     print("Flashing Board over PCIe...")
-    subprocess.run(f"cd software/user && ./m2sdr_util flash_write ../../{args.bitstream} {args.offset}", shell=True)
+    subprocess.run(f"cd software/user && ./m2sdr_util flash_write ../../{bitstream} {offset}", shell=True)
     subprocess.run("cd software/user && ./m2sdr_util flash_reload", shell=True)
     time.sleep(1)
 
+def remove_driver():
     print("Removing Driver...")
     subprocess.run("sudo rmmod litepcie", shell=True)
-    device_ids = [
-        get_pcie_device_id("0x10ee", "0x7021"),
-        get_pcie_device_id("0x10ee", "0x7022"),
-        get_pcie_device_id("0x10ee", "0x7024"),
-    ]
 
+def remove_board_from_pcie_bus(device_ids):
     print("Removing Board from PCIe Bus...")
     for device_id in device_ids:
         if device_id:
             remove_pcie_device(device_id)
 
+def rescan_bus():
     print("Rescanning PCIe Bus...")
     rescan_pcie_bus()
 
+def load_driver():
     print("Loading Driver...")
     subprocess.run("sudo sh -c 'cd software/kernel && ./init.sh'", shell=True)
 
-    print("Done!")
+def get_device_ids():
+    return [
+        get_pcie_device_id("0x10ee", "0x7021"),
+        get_pcie_device_id("0x10ee", "0x7022"),
+        get_pcie_device_id("0x10ee", "0x7024"),
+    ]
+
+# Main ----------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="FPGA flashing over PCIe.")
+    parser.add_argument('bitstream', help='Path to the bitstream file')
+    parser.add_argument('-o', '--offset', type=lambda x: int(x, 0), default=0x00000000, help='Offset for flashing (default: 0x00000000)')
+    args = parser.parse_args()
+
+    # Flash.
+    flash_bitstream(args.bitstream, args.offset)
+
+    # PCIe Rescan and driver Remove/Reload.
+    remove_driver()
+    device_ids = get_device_ids()
+    remove_board_from_pcie_bus(device_ids)
+    rescan_bus()
+    load_driver()
 
 if __name__ == '__main__':
     main()
