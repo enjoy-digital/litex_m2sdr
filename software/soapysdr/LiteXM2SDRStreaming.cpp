a5617ccff78772e4682134e6f9e5290744f7be73
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-09-05 19:13:42 +0200

    software/soapysdr/LiteXM2SDRStreaming.cpp: improved code readability

--- software/soapysdr/LiteXM2SDRStreaming.cpp
+++ software/soapysdr/LiteXM2SDRStreaming.cpp
@@ -119,10 +119,17 @@ SoapySDR::Stream *SoapyLiteXM2SDR::setupStream(
 
     /* Configure 2T2R/1T1R mode (PHY) */
     litepcie_writel(_fd, CSR_AD9361_PHY_CONTROL_ADDR, _nChannels == 1 ? 1 : 0);
-    if (direction == SOAPY_SDR_RX)
-        ad9361_en_dis_rx(ad9361_phy, RX_1 | RX_2, _nChannels == 2 ? RX_1 | RX_2 : (_rx_stream.channels[0] == 0 ? RX_1 : RX_2));
-    else if (direction == SOAPY_SDR_TX)
-        ad9361_en_dis_tx(ad9361_phy, TX_1 | TX_2, _nChannels == 2 ? TX_1 | TX_2 : (_tx_stream.channels[0] == 0 ? TX_1 : TX_2));
+    if (_nChannels == 1) {
+        if (direction == SOAPY_SDR_RX)
+            ad9361_en_dis_rx(ad9361_phy, RX_1 | RX_2, _rx_stream.channels[0] == 0 ? RX_1 : RX_2);
+        else if (direction == SOAPY_SDR_TX)
+            ad9361_en_dis_tx(ad9361_phy, TX_1 | TX_2, _tx_stream.channels[0] == 0 ? TX_1 : TX_2);
+    } else {
+        if (direction == SOAPY_SDR_RX)
+            ad9361_en_dis_rx(ad9361_phy, RX_1 | RX_2, RX_1 | RX_2);
+        else if (direction == SOAPY_SDR_TX)
+            ad9361_en_dis_tx(ad9361_phy, TX_1 | TX_2, TX_1 | TX_2);
+    }
     return direction == SOAPY_SDR_RX ? RX_STREAM : TX_STREAM;
 }
 
