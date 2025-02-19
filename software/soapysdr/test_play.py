fc5c4eccaa8af5f6add00a648f7511f970b1b9e2
Florent Kermarrec <florent@enjoy-digital.fr>
2025-02-19 09:13:14 +0100

    software/soapysdr/test_play.py: Add --channel support.

--- software/soapysdr/test_play.py
+++ software/soapysdr/test_play.py
@@ -5,16 +5,16 @@ test_play.py - Transmit I/Q samples using the LiteXM2SDR SoapySDR driver.
 
 This script uses the SoapySDR driver to interface with the LiteXM2SDR hardware. It can either
 generate a continuous tone or play back a file of raw CF32 samples. Command-line options allow you
-to configure sample rate, bandwidth, frequency, and gain. In file playback mode, the file can be
-looped a specified number of times.
+to configure sample rate, bandwidth, frequency, gain, and channel. In file playback mode, the file
+can be looped a specified number of times.
 
 Usage Examples:
 
 Tone Generation:
-    ./test_play.py --samplerate 4e6 --bandwidth 56e6 --freq 2.4e9 --gain -20 --tone-freq 1e6 --ampl 0.8 --secs 5
+    ./test_play.py --samplerate 4e6 --bandwidth 56e6 --freq 2.4e9 --gain -20 --channel 0 --tone-freq 1e6 --ampl 0.8 --secs 5
 
 File Playback:
-    ./test_play.py --samplerate 4e6 --bandwidth 56e6 --freq 2.4e9 --gain -20 path/to/file.bin 10
+    ./test_play.py --samplerate 4e6 --bandwidth 56e6 --freq 2.4e9 --gain -20 --channel 0 path/to/file.bin 10
 """
 
 import time
@@ -56,10 +56,11 @@ def main():
         formatter_class = argparse.ArgumentDefaultsHelpFormatter,
     )
     # RF configuration options.
-    parser.add_argument("--samplerate", type=float, default=4e6,   help="TX Sample rate in Hz")
-    parser.add_argument("--bandwidth",  type=float, default=56e6,  help="TX Filter bandwidth in Hz")
-    parser.add_argument("--freq",       type=float, default=2.4e9, help="TX frequency in Hz")
-    parser.add_argument("--gain",       type=float, default=-20.0, help="TX gain in dB")
+    parser.add_argument("--samplerate", type=float, default=4e6,    help="TX Sample rate in Hz")
+    parser.add_argument("--bandwidth",  type=float, default=56e6,   help="TX Filter bandwidth in Hz")
+    parser.add_argument("--freq",       type=float, default=2.4e9,  help="TX frequency in Hz")
+    parser.add_argument("--gain",       type=float, default=-20.0,  help="TX gain in dB")
+    parser.add_argument("--channel",    type=int,   choices=[0, 1], default=0, help="TX channel index (0 or 1)")
 
     # Transmission mode: tone generation or file playback.
     group = parser.add_mutually_exclusive_group(required=True)
@@ -76,11 +77,11 @@ def main():
     # Open the LiteXM2SDR device using the SoapySDR driver.
     sdr = SoapySDR.Device({"driver": "LiteXM2SDR"})
 
-    # Basic RF configuration
-    sdr.setSampleRate(SOAPY_SDR_TX, 0, args.samplerate)
-    sdr.setFrequency( SOAPY_SDR_TX, 0, args.freq)
-    sdr.setGain(      SOAPY_SDR_TX, 0, args.gain)
-    sdr.setBandwidth( SOAPY_SDR_TX, 0, args.bandwidth)
+    # Basic RF configuration using the selected channel.
+    sdr.setSampleRate(SOAPY_SDR_TX, args.channel, args.samplerate)
+    sdr.setFrequency( SOAPY_SDR_TX, args.channel, args.freq)
+    sdr.setGain(      SOAPY_SDR_TX, args.channel, args.gain)
+    sdr.setBandwidth( SOAPY_SDR_TX, args.channel, args.bandwidth)
 
     # Determine the data source based on mode.
     if args.tone_freq is not None:
@@ -96,15 +97,15 @@ def main():
                     yield chunk
         mode = "file"
 
-    # Create and activate TX stream.
-    tx_stream = sdr.setupStream(SOAPY_SDR_TX, SOAPY_SDR_CF32, [0])
+    # Create and activate TX stream on the specified channel.
+    tx_stream = sdr.setupStream(SOAPY_SDR_TX, SOAPY_SDR_CF32, [args.channel])
     sdr.activateStream(tx_stream)
 
     t_start = time.time()
 
     # Play Tone.
     if mode == "tone":
-        print(f"Playing tone for {args.secs} seconds at {args.freq/1e6:.3f} MHz...")
+        print(f"Playing tone for {args.secs} seconds at {args.freq/1e6:.3f} MHz on channel {args.channel}...")
         for samples in get_samples():
             if time.time() - t_start > args.secs:
                 break
@@ -120,7 +121,7 @@ def main():
 
     # Play File.
     else:
-        print(f"Playing file '{args.filename}' for {args.loops} loop(s) at {args.freq/1e6:.3f} MHz...")
+        print(f"Playing file '{args.filename}' for {args.loops} loop(s) at {args.freq/1e6:.3f} MHz on channel {args.channel}...")
         for samples in get_samples():
             sr = sdr.writeStream(tx_stream, [samples], len(samples))
             if sr.ret < 0:
