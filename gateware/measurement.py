ec4fc3c36f57c00fd620410bb3ebc0e5f9f7e739
Florent Kermarrec <florent@enjoy-digital.fr>
2025-02-28 21:38:02 +0100

    gateware/measurement: Make CSR optional and add an optional latch_all CSR to latch all clocks simultaneously.

--- gateware/measurement.py
+++ gateware/measurement.py
@@ -15,9 +15,9 @@ from litex.soc.interconnect.csr import *
 # Clk Measurement ----------------------------------------------------------------------------------
 
 class ClkMeasurement(LiteXModule):
-    def __init__(self, clk, increment=1):
-        self.latch = CSR()
-        self.value = CSRStatus(64)
+    def __init__(self, clk, increment=1, with_csr=True):
+        self.latch = Signal()
+        self.value = Signal(64)
 
         # # #
 
@@ -34,14 +34,36 @@ class ClkMeasurement(LiteXModule):
         latch_value = Signal(64)
         latch_sync  = PulseSynchronizer("sys", "counter")
         self.submodules += latch_sync
-        self.comb += latch_sync.i.eq(self.latch.re)
+        self.comb += latch_sync.i.eq(self.latch)
         self.sync.counter += If(latch_sync.o, latch_value.eq(counter))
-        self.specials += MultiReg(latch_value, self.value.status)
+        self.specials += MultiReg(latch_value, self.value)
+
+        # CSR (Optional).
+        if with_csr:
+            self.add_csr()
+
+    def add_csr(self):
+        self._latch = CSR()
+        self._value = CSRStatus(64)
+        self.comb += [
+            If(self._latch.re, self.latch.eq(1)),
+            self._value.status.eq(self.value),
+        ]
 
 # Multi Clk Measurement ----------------------------------------------------------------------------
 
 class MultiClkMeasurement(LiteXModule):
-    def __init__(self, clks):
+    def __init__(self, clks, with_csr=True, with_latch_all=True):
         assert isinstance(clks, dict)
+
+        # Clock Measurement Modules.
+        self.clk_modules = {}
         for name, clk in clks.items():
-            self.add_module(name=name, module=ClkMeasurement(clk))
+            self.clk_modules[name] = ClkMeasurement(clk, with_csr=with_csr)
+            self.add_module(name=name, module=self.clk_modules[name])
+
+        # Latch All CSR (Optional)
+        if with_csr and with_latch_all:
+            self.latch_all = CSR()
+            for name, mod in self.clk_modules.items():
+                self.comb += If(self.latch_all.re, mod.latch.eq(1))
