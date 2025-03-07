3b51936145f05cf389310b7d92e26f4a75fe7a00
Florent Kermarrec <florent@enjoy-digital.fr>
2025-03-07 08:56:02 +0100

    software/user/libm2sdr/m2sdr_ad9361_spi.c: Don't do final read in spi_xfer is xfer is a write and add comments.

--- software/user/libm2sdr/m2sdr_ad9361_spi.c
+++ software/user/libm2sdr/m2sdr_ad9361_spi.c
@@ -40,10 +40,23 @@ void m2sdr_ad9361_spi_init(int fd, uint8_t reset) {
 }
 
 void m2sdr_ad9361_spi_xfer(int fd, uint8_t len, uint8_t *mosi, uint8_t *miso) {
+    /* Check write. */
+    bool is_write = (mosi[0] & 0x80) != 0;
+
+    /* Write MOSI. */
     litepcie_writel(fd, CSR_AD9361_SPI_MOSI_ADDR, mosi[0] << 16 | mosi[1] << 8 | mosi[2]);
+
+    /* Start SPI. */
     litepcie_writel(fd, CSR_AD9361_SPI_CONTROL_ADDR, 24*SPI_CONTROL_LENGTH | SPI_CONTROL_START);
+
+    /* Wait done. */
     while ((litepcie_readl(fd, CSR_AD9361_SPI_STATUS_ADDR) & 0x1) != SPI_STATUS_DONE);
-    miso[2] = litepcie_readl(fd, CSR_AD9361_SPI_MISO_ADDR) & 0xff;
+
+    /* Read MISO if read. */
+    miso[2] = 0;
+    if (!is_write) {
+        miso[2] = litepcie_readl(fd, CSR_AD9361_SPI_MISO_ADDR) & 0xff;
+    }
 }
 
 void m2sdr_ad9361_spi_write(int fd, uint16_t reg, uint8_t dat) {
@@ -107,10 +120,23 @@ void m2sdr_ad9361_eb_spi_init(struct eb_connection *fd, uint8_t reset) {
 }
 
 void m2sdr_ad9361_eb_spi_xfer(struct eb_connection *eb, uint8_t len, uint8_t *mosi, uint8_t *miso) {
+    /* Check write. */
+    bool is_write = (mosi[0] & 0x80) != 0;
+
+    /* Send MOSI. */
     eb_writel(eb, CSR_AD9361_SPI_MOSI_ADDR, mosi[0] << 16 | mosi[1] << 8 | mosi[2]);
+
+    /* Start SPI. */
     eb_writel(eb, CSR_AD9361_SPI_CONTROL_ADDR, 24*SPI_CONTROL_LENGTH | SPI_CONTROL_START);
+
+    /* Wait done. */
     while ((eb_read32(eb, CSR_AD9361_SPI_STATUS_ADDR) & 0x1) != SPI_STATUS_DONE);
-    miso[2] = eb_read32(eb, CSR_AD9361_SPI_MISO_ADDR) & 0xff;
+
+    /* Read MISO if read. */
+    miso[2] = 0;
+    if (!is_write) {
+        miso[2] = eb_read32(eb, CSR_AD9361_SPI_MISO_ADDR) & 0xff;
+    }
 }
 
 void m2sdr_ad9361_eb_spi_write(struct eb_connection *eb, uint16_t reg, uint8_t dat) {
