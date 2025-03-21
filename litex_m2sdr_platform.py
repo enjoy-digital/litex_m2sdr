663f944d2d196dfcba1af31fd712f286b71b6055
Florent Kermarrec <florent@enjoy-digital.fr>
2025-03-21 10:42:43 +0100

    platform: Switch CONFIGRATE to 33MHz since 66MHz seems to high on some boards.

--- litex_m2sdr_platform.py
+++ litex_m2sdr_platform.py
@@ -191,7 +191,7 @@ class Platform(Xilinx7SeriesPlatform):
         self.toolchain.bitstream_commands = [
             "set_property BITSTREAM.CONFIG.UNUSEDPIN Pulldown [current_design]",
             "set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]",
-            "set_property BITSTREAM.CONFIG.CONFIGRATE 66 [current_design]",
+            "set_property BITSTREAM.CONFIG.CONFIGRATE 33 [current_design]",
             "set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]",
             "set_property CFGBVS VCCO [current_design]",
             "set_property CONFIG_VOLTAGE 3.3 [current_design]",
