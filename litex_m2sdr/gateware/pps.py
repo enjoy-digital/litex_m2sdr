54a65a9d757a093e6abeaf4488e0d01054d57ed8
Florent Kermarrec <florent@enjoy-digital.fr>
2025-08-19 09:25:46 +0200

    gateware/pps: Switch to optimized version (No DSPs, better timings) and 100ms pulse instead (More usual) instead of 200ms pulse.

--- litex_m2sdr/gateware/pps.py
+++ litex_m2sdr/gateware/pps.py
@@ -15,32 +15,53 @@ from litex.gen.genlib.misc import WaitTimer
 # PPS Generator ------------------------------------------------------------------------------------
 
 class PPSGenerator(LiteXModule):
+    """
+    PPS Generator Module.
+
+    Generates a Pulse Per Second (PPS) signal with a 10% high / 90% low duty cycle based on a
+    specified time input and clock frequency. Includes an offset to adjust the timing.
+    """
     def __init__(self, clk_freq, time, offset=int(500e6), reset=0):
-        self.pps    = Signal() # PPS Output.
+        # IOs.
+        self.pps = pps = Signal()  # PPS output signal.
 
         # # #
 
-        # PPS Signals.
-        self.start = start = Signal()
-        self.count = count = Signal(32)
+        # Parameters.
+        # -----------
+        self.clk_freq = clk_freq  # System clock frequency in Hz.
+        self.offset   = offset    # Timing offset in ns.
 
-        # PPS FSM.
+        # Signals.
+        # --------
+        self.start     = start     = Signal()   # Trigger for PPS pulse.
+        self.time_next = time_next = Signal(64) # Next Time Value for PPS pulse.
+
+        # FSM.
+        # ----
         self.fsm = fsm = ResetInserter()(FSM(reset_state="IDLE"))
         self.comb += fsm.reset.eq(reset)
+
+        # Idle State: Wait for valid time input.
         fsm.act("IDLE",
-            NextValue(count, 0),
+            NextValue(time_next, int(1e9) + offset),
             If(time != 0,
                 NextState("RUN")
             )
         )
+
+        # Run State: Count and trigger PPS.
         fsm.act("RUN",
-            If(time > ((count * int(1e9) + offset)),
+            If(time > time_next,
                 start.eq(1),
-                NextValue(count, count + 1)
+                NextValue(time_next, time_next + int(1e9))
             )
         )
 
-        # PPS Generation.
-        self.timer = WaitTimer(clk_freq*200e-3) # 20% High / 80% Low PPS.
-        self.comb += self.timer.wait.eq(~start)
-        self.comb += self.pps.eq(~self.timer.done)
+        # PPS Pulse Generation.
+        # --------------------
+        self.timer = WaitTimer(int(clk_freq * 100e-3))  # 10% high pulse width.
+        self.comb += [
+            self.timer.wait.eq(~start),
+            pps.eq(~self.timer.done)
+        ]
