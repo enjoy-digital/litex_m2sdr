9996d2979f44c2e7c9e8b46626eb0f65e8dde35c
Florent Kermarrec <florent@enjoy-digital.fr>
2025-08-19 17:10:04 +0200

    platform: Enable power down on overtemp.

--- litex_m2sdr_platform.py
+++ litex_m2sdr_platform.py
@@ -194,6 +194,7 @@ class Platform(Xilinx7SeriesPlatform):
             "set_property BITSTREAM.CONFIG.UNUSEDPIN Pulldown [current_design]",
             "set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]",
             "set_property BITSTREAM.CONFIG.CONFIGRATE 33 [current_design]",
+            "set_property BITSTREAM.CONFIG.OVERTEMPPOWERDOWN ENABLE [current_design]",
             "set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]",
             "set_property CFGBVS VCCO [current_design]",
             "set_property CONFIG_VOLTAGE 3.3 [current_design]",
