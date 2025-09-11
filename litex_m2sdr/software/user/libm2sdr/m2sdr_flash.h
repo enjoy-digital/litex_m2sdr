8c7f274ef47cb76fc2ab8e385418533467aec07b
Florent Kermarrec <florent@enjoy-digital.fr>
2025-09-11 11:33:05 +0200

    software/user/libm2sdr/m2sdr_flash.h: Cleanup and use coding style similar to other lib files.

--- litex_m2sdr/software/user/libm2sdr/m2sdr_flash.h
+++ litex_m2sdr/software/user/libm2sdr/m2sdr_flash.h
@@ -1,11 +1,10 @@
-/* SPDX-License-Identifier: BSD-2-Clause
- *
+/*
  * LiteX-M2SDR library
  *
- * This file is part of LiteX-M2SDR.
+ * This file is part of LiteX-M2SDR project.
  *
- * Copyright (C) 2024-2025 Enjoy-Digital
- *
+ * Copyright (c) 2024-2025 Enjoy-Digital <enjoy-digital.fr>
+ * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #ifndef M2SDR_LIB_FLASH_H
@@ -13,24 +12,39 @@
 
 #include <stdint.h>
 
-#define SPI_CTRL_START  (1 << 0)
-#define SPI_CTRL_LENGTH (1 << 8)
-#define SPI_STATUS_DONE (1 << 0)
+/* SPI Constants */
+/*---------------*/
 
-#define FLASH_READ_ID_REG 0x9F
+#define SPI_CTRL_START   (1 << 0)
+#define SPI_CTRL_LENGTH  (1 << 8)
+#define SPI_STATUS_DONE  (1 << 0)
 
-#define FLASH_READ    0x03
-#define FLASH_WREN    0x06
-#define FLASH_WRDI    0x04
-#define FLASH_PP      0x02
-#define FLASH_SE      0xD8
-#define FLASH_BE      0xC7
-#define FLASH_RDSR    0x05
-#define FLASH_WRSR    0x01
-/* status */
-#define FLASH_WIP     0x01
+/* Flash Commands */
+/*----------------*/
 
-#define FLASH_SECTOR_SIZE (1 << 16)
+#define FLASH_READ_ID_REG  0x9F
+
+#define FLASH_READ         0x03
+#define FLASH_WREN         0x06
+#define FLASH_WRDI         0x04
+#define FLASH_PP           0x02
+#define FLASH_SE           0xD8
+#define FLASH_BE           0xC7
+#define FLASH_RDSR         0x05
+#define FLASH_WRSR         0x01
+
+/* Flash Status */
+/*--------------*/
+
+#define FLASH_WIP          0x01
+
+/* Flash Geometry */
+/*----------------*/
+
+#define FLASH_SECTOR_SIZE  (1 << 16)
+
+/* Flash Functions */
+/*-----------------*/
 
 uint8_t m2sdr_flash_read(void *conn, uint32_t addr);
 int m2sdr_flash_get_erase_block_size(void *conn);
@@ -39,4 +53,4 @@ int m2sdr_flash_write(void *conn,
                       void (*progress_cb)(void *opaque, const char *fmt, ...),
                       void *opaque);
 
-#endif // M2SDR_LIB_FLASH_H
+#endif /* M2SDR_LIB_FLASH_H */
