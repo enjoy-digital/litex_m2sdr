c84da0a00ed6ce761f36bc41f184c693cd7e07e7
Florent Kermarrec <florent@enjoy-digital.fr>
2025-10-22 15:37:35 +0200

    software/kernel/main: Integrate litesata.c driver.

--- litex_m2sdr/software/kernel/main.c
+++ litex_m2sdr/software/kernel/main.c
@@ -46,6 +46,7 @@
 #include "config.h"
 #include "flags.h"
 #include "soc.h"
+#include "mem.h"
 
 //#define DEBUG_CSR
 //#define DEBUG_MSI
@@ -55,6 +56,9 @@
 
 extern int liteuart_init(void);
 extern void liteuart_exit(void);
+
+extern int litesata_init(void);
+extern void litesata_exit(void);
 
 #define LITEPCIE_NAME "m2sdr"
 #define LITEPCIE_MINOR_COUNT 32
@@ -104,6 +108,7 @@ struct litepcie_chan {
 struct litepcie_device {
 	struct pci_dev *dev;                          /* PCI device */
 	struct platform_device *uart;                 /* UART platform device */
+	struct platform_device *sata;                 /* SATA platform device */
 	resource_size_t bar0_size;                    /* Size of BAR0 */
 	phys_addr_t bar0_phys_addr;                   /* Physical address of BAR0 */
 	uint8_t *bar0_addr;                           /* Virtual address of BAR0 */
@@ -1519,6 +1524,7 @@ static int litepcie_pci_probe(struct pci_dev *dev, const struct pci_device_id *i
 		goto fail3;
 	}
 
+/* LiteUART platform device */
 #ifdef CSR_UART_XOVER_RXTX_ADDR
 	tty_res = devm_kzalloc(&dev->dev, sizeof(struct resource), GFP_KERNEL);
 	if (!tty_res)
@@ -1531,6 +1537,51 @@ static int litepcie_pci_probe(struct pci_dev *dev, const struct pci_device_id *i
 	if (IS_ERR(litepcie_dev->uart)) {
 		ret = PTR_ERR(litepcie_dev->uart);
 		goto fail3;
+	}
+#endif
+
+/* LiteSATA platform device */
+#ifdef CSR_SATA_PHY_BASE
+	{
+		struct resource res[9];
+		struct platform_device *sata_pdev;
+		resource_size_t base = (resource_size_t)litepcie_dev->bar0_addr;
+
+#define FILL_REG_RES(_idx,_name,_csr,_size)			\
+		do {						\
+			res[_idx].name  = (_name);		\
+			res[_idx].flags = IORESOURCE_REG;	\
+			res[_idx].start = base + (_csr) - CSR_BASE; \
+			res[_idx].end   = res[_idx].start + (_size) - 1; \
+		} while (0)
+
+		FILL_REG_RES(0, "ident",  CSR_SATA_IDENTIFY_BASE,   0x100);
+		FILL_REG_RES(1, "phy",    CSR_SATA_PHY_BASE,        0x100);
+		FILL_REG_RES(2, "reader", CSR_SATA_SECTOR2MEM_BASE, 0x100);
+		FILL_REG_RES(3, "writer", CSR_SATA_MEM2SECTOR_BASE, 0x100);
+		FILL_REG_RES(4, "irq",    CSR_SATA_IRQ_BASE,        0x100);
+		FILL_REG_RES(5, "buf_rd", SATA_RD_BASE,             SATA_RD_SIZE);
+		FILL_REG_RES(6, "buf_wr", SATA_WR_BASE,             SATA_WR_SIZE);
+
+#undef FILL_REG_RES
+
+		res[7].name  = "buf_rd_bus";
+		res[7].flags = IORESOURCE_MEM;
+		res[7].start = SATA_RD_BASE;
+		res[7].end   = SATA_RD_BASE + SATA_RD_SIZE - 1;
+
+		res[8].name  = "buf_wr_bus";
+		res[8].flags = IORESOURCE_MEM;
+		res[8].start = SATA_WR_BASE;
+		res[8].end   = SATA_WR_BASE + SATA_WR_SIZE - 1;
+
+		sata_pdev = platform_device_register_simple("litesata", 1, res, ARRAY_SIZE(res));
+		if (IS_ERR(sata_pdev))
+			dev_warn(&dev->dev, "LiteSATA pdev registration failed: %ld\n", PTR_ERR(sata_pdev));
+		else {
+			dev_info(&dev->dev, "LiteSATA platform device registered (polling mode)\n");
+			litepcie_dev->sata = sata_pdev;
+		}
 	}
 #endif
 
@@ -1613,6 +1664,12 @@ static void litepcie_pci_remove(struct pci_dev *dev)
 #ifdef CSR_UART_XOVER_RXTX_ADDR
 	platform_device_unregister(litepcie_dev->uart);
 #endif
+#ifdef CSR_SATA_PHY_BASE
+	if (litepcie_dev->sata) {
+		platform_device_unregister(litepcie_dev->sata);
+		litepcie_dev->sata = NULL;
+	}
+#endif
 
 	litepcie_free_chdev(litepcie_dev);
 
@@ -1682,6 +1739,11 @@ static int __init litepcie_module_init(void)
 	if (res)
 		return res;
 
+	res = litesata_init();
+	if (res) {
+		return res;
+	}
+
 	#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 4, 0)
 		litepcie_class = class_create(THIS_MODULE, LITEPCIE_NAME);
 	#else
@@ -1725,6 +1787,7 @@ static void __exit litepcie_module_exit(void)
 	class_destroy(litepcie_class);
 
 	liteuart_exit();
+	litesata_exit();
 }
 
 module_init(litepcie_module_init);
