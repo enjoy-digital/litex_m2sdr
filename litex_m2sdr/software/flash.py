8102df104cf00e243af3b5b0b18a2302a7bedb3b
ismailessaidi11 <ismailessaidi11@gmail.com>
2025-10-27 16:23:32 +0100

    small upgrades to flash.py

--- litex_m2sdr/software/flash.py
+++ litex_m2sdr/software/flash.py
@@ -27,31 +27,28 @@ def main():
     parser.add_argument("-r", "--rescan",     action="store_true",      default=True,       help="Enable PCIe rescan after flashing.")
     args = parser.parse_args()
 
-    # Ask for confirmation before flashing.
-    confirm = input("Are you sure you want to flash the bitstream? (yes/no): ")
-    if confirm.lower() not in ["yes", "y"]:
-        print("Flashing aborted.")
-        return
-
-    # Ask for confirmation when non-default offset.
-    if args.offset != 0x00800000:
-        confirm = input(f"You are flashing bitstream at non-default Operational offset: 0x{args.offset:08x} vs 0x00800000, are you sure? (yes/no): ")
-        if confirm.lower() not in ["yes", "y"]:
-            print("Flashing aborted.")
-            return
-    
     # Warn when flashing non-default bitstream (file )
-    if "litex_m2sdr_m2_pcie_x1_operational.bin" not in str(args.bitstream):
+    if "litex_m2sdr_m2_pcie_x1_operational.bin" in str(args.bitstream) and args.offset != 0x00800000:
         # print in red the warning 
         print("\033[91m", end="")
-        print(f"Warning: You are flashing a wrong bitstream: {args.bitstream}")
+        print(f"Warning: You are flashing a multiboot bitstream: {args.bitstream} in non-default Operational offset: 0x{args.offset:08x}")
         print("\033[0m", end="")
-        confirm = input("Are you sure you want to continue? (yes/no): ")
+        print(f"Add -o 0x00800000 to flash at the correct offset.")
+        print("Flashing aborted.")
+        return
+    elif (("litex_m2sdr_m2_pcie_x1.bin" in str(args.bitstream)) or ("litex_m2sdr_m2_pcie_x1_fallback.bin" in str(args.bitstream))) and args.offset != 0x00000000:
+        print("\033[91m", end="")
+        print(f"Warning: You are flashing a non-multiboot bitstream: {args.bitstream} in non-default offset: 0x{args.offset:08x}")
+        print("\033[0m", end="")
+        print(f"Add -o 0x00000000 to flash at the correct offset.")
+        print("Flashing aborted.")
+        return
+    else:
+        confirm = input(f"Are you sure you want to flash the bitstream {args.bitstream} at offset 0x{args.offset:08x} ? \n(yes/no): ")
         if confirm.lower() not in ["yes", "y"]:
             print("Flashing aborted.")
             return
-    else:
-        print(f"Flashing default bitstream: {args.bitstream}")
+
 
     # Flash with selected device.
     flash_bitstream(args.bitstream, args.offset, args.device_num)
