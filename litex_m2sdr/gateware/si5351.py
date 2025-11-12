9798fd7af768e629dc68e63d34ee642ca7b75a1f
Florent Kermarrec <florent@enjoy-digital.fr>
2025-11-12 14:26:34 +0100

    gateware/si5351: Update SI5351B-C Default Config (to be similar to default software config).

--- litex_m2sdr/gateware/si5351.py
+++ litex_m2sdr/gateware/si5351.py
@@ -40,34 +40,34 @@ SI5351_CLKIN_FROM_UFL  = 0b1
 
 # SI5351B-C Default Config from XO (38.4MHz on MS0/2/3/4/5/6/7 and 100MHz on MS1).
 SI5351_I2C_SEQUENCE = [
-    # Interrupt Mask Configuration.
-    ( 0x02, 0x33 ),  # Int masks: CLK_LOS(1), LOL_A(1) enabled, XO_LOS(0), LOL_B(0), SYS_INIT(0) disabled.
+    # Interrupt Mask Configuration
+    ( 0x02, 0x33 ),  # Int masks: CLK_LOS(1), LOL_A(1) enabled, XO_LOS(0), LOL_B(0), SYS_INIT(0) disabled
 
-    # Output Enable Control.
-    ( 0x03, 0x00 ),  # All CLK outputs enabled via register (OEB pin disabled).
+    # Output Enable Control
+    ( 0x03, 0x00 ),  # All CLK outputs enabled via register (OEB pin disabled)
 
-    # PLL Reset Control.
-    ( 0x04, 0x10 ),  # Disable reset on PLLA LOS (bit4=1), PLLB reset normal (bit5=0).
+    # PLL Reset Control
+    ( 0x04, 0x10 ),  # Disable reset on PLLA LOS (bit4=1), PLLB reset normal (bit5=0)
 
-    # I2C Configuration.
-    ( 0x07, 0x01 ),  # I2C address: 0x60 (default).
+    # I2C Configuration
+    ( 0x07, 0x01 ),  # I2C address: 0x60 (default)
 
-    # Clock Input Configuration.
-    ( 0x0F, 0x00 ),  # PLL src=XTAL (25MHz), CLKIN_DIV=1.
+    # Clock Input Configuration
+    ( 0x0F, 0x00 ),  # PLLB, src=XTAL (25MHz)
 
-    # Output Channel Configuration (CLK0-CLK7).
-    ( 0x10, 0x2F ),  # CLK0: LVCMOS 8mA, MS0 src=PLLB.
-    ( 0x11, 0x2F ),  # CLK1: LVCMOS 8mA, MS1 src=PLLB.
-    ( 0x12, 0x2F ),  # CLK2: LVCMOS 8mA, MS2 src=PLLB.
-    ( 0x13, 0x2F ),  # CLK3: LVCMOS 8mA, MS3 src=PLLB.
-    ( 0x14, 0x2F ),  # CLK4: LVCMOS 8mA, MS4 src=PLLB.
-    ( 0x15, 0x2F ),  # CLK5: LVCMOS 8mA, MS5 src=PLLB.
-    ( 0x16, 0x2F ),  # CLK6: LVCMOS 8mA, MS6 src=PLLB.
-    ( 0x17, 0x2F ),  # CLK7: LVCMOS 8mA, MS7 src=PLLB.
+    # Output Channel Configuration (CLK0-CLK7)
+    ( 0x10, 0x2D ),  # CLK0: LVCMOS 4mA, MS0 src=PLLB
+    ( 0x11, 0x2D ),  # CLK1: LVCMOS 4mA, MS1 src=PLLB
+    ( 0x12, 0x8C ),  # CLK2: OFF.
+    ( 0x13, 0x8C ),  # CLK3: OFF.
+    ( 0x14, 0x2D ),  # CLK4: LVCMOS 4mA, MS4 src=PLLB
+    ( 0x15, 0x8C ),  # CLK5: OFF.
+    ( 0x16, 0x8C ),  # CLK6: OFF.
+    ( 0x17, 0x8C ),  # CLK7: OFF.
 
-    # PLLB Configuration (VCO = 844.8MHz from 25MHz XTAL).
-    ( 0x22, 0x42 ),  # PLLB feedback: Multisynth NA (integer mode).
-    ( 0x23, 0x40 ),  # PLLB reset=normal.
+    # PLLB Configuration (VCO = 844.8MHz from 25MHz XTAL)
+    ( 0x22, 0x42 ),
+    ( 0x23, 0x40 ),
     ( 0x24, 0x00 ),
     ( 0x25, 0x0E ),
     ( 0x26, 0xE5 ),
@@ -75,7 +75,7 @@ SI5351_I2C_SEQUENCE = [
     ( 0x28, 0xBC ),
     ( 0x29, 0xC0 ),
 
-    # MS0 Configuration (Output Divider 22 for 38.4MHz).
+    # MS0 Configuration (Output Divider 22 for 38.4MHz)
     ( 0x2A, 0x00 ),
     ( 0x2B, 0x01 ),
     ( 0x2C, 0x00 ),
@@ -85,7 +85,7 @@ SI5351_I2C_SEQUENCE = [
     ( 0x30, 0x00 ),
     ( 0x31, 0x00 ),
 
-    # MS1 Configuration (Output Divider 8.448 for 100MHz).
+    # MS1 Configuration (Output Divider 8.448 for 100MHz)
     ( 0x32, 0x00 ),
     ( 0x33, 0x7D ),
     ( 0x34, 0x00 ),
@@ -95,27 +95,7 @@ SI5351_I2C_SEQUENCE = [
     ( 0x38, 0x00 ),
     ( 0x39, 0x2B ),
 
-    # MS2 Configuration (Output Divider 22 for 38.4MHz).
-    ( 0x3A, 0x00 ),
-    ( 0x3B, 0x01 ),
-    ( 0x3C, 0x00 ),
-    ( 0x3D, 0x09 ),
-    ( 0x3E, 0x00 ),
-    ( 0x3F, 0x00 ),
-    ( 0x40, 0x00 ),
-    ( 0x41, 0x00 ),
-
-    # MS3 Configuration (Output Divider 22 for 38.4MHz).
-    ( 0x42, 0x00 ),
-    ( 0x43, 0x01 ),
-    ( 0x44, 0x00 ),
-    ( 0x45, 0x09 ),
-    ( 0x46, 0x00 ),
-    ( 0x47, 0x00 ),
-    ( 0x48, 0x00 ),
-    ( 0x49, 0x00 ),
-
-    # MS4 Configuration (Output Divider 22 for 38.4MHz).
+    # MS4 Configuration (Output Divider 22 for 38.4MHz)
     ( 0x4A, 0x00 ),
     ( 0x4B, 0x01 ),
     ( 0x4C, 0x00 ),
@@ -125,46 +105,30 @@ SI5351_I2C_SEQUENCE = [
     ( 0x50, 0x00 ),
     ( 0x51, 0x00 ),
 
-    # MS5 Configuration (Output Divider 22 for 38.4MHz).
-    ( 0x52, 0x00 ),
-    ( 0x53, 0x01 ),
-    ( 0x54, 0x00 ),
-    ( 0x55, 0x09 ),
-    ( 0x56, 0x00 ),
-    ( 0x57, 0x00 ),
-    ( 0x58, 0x00 ),
-    ( 0x59, 0x00 ),
+    # Spread-Spectrum, Fractional Stepping Disabled
+    ( 0x95, 0x00 ),
+    ( 0x96, 0x00 ),
+    ( 0x97, 0x00 ),
+    ( 0x98, 0x00 ),
+    ( 0x99, 0x00 ),
+    ( 0x9A, 0x00 ),
+    ( 0x9B, 0x00 ),
 
-    # MS6 Configuration (Output Divider 22 for 38.4MHz).
-    ( 0x5A, 0x16 ),
-
-    # MS7 Configuration (Output Divider 22 for 38.4MHz).
-    ( 0x5B, 0x16 ),
-
-    # Spread-Spectrum, Fractional Stepping Disabled.
-    ( 0x95, 0x00 ),  # SSDN_P2=0, SSC_EN=0 => no SS.
-    ( 0x96, 0x00 ),  # Reserved = 0.
-    ( 0x97, 0x00 ),  # SSDN_P3=0, SSC_MODE=0 => off.
-    ( 0x98, 0x00 ),  # Reserved = 0.
-    ( 0x99, 0x00 ),  # SSDN_P1=0.
-    ( 0x9A, 0x00 ),  # SSUDP=0 => no up/down spread.
-    ( 0x9B, 0x00 ),  # Reserved = 0.
-
-    # VCXO Configuration.
+    # VCXO Configuration
     ( 0xA2, 0xF2 ),
     ( 0xA3, 0xFD ),
     ( 0xA4, 0x01 ),
 
-    # Phase Offset Configuration.
-    ( 0xA5, 0x00 ),  # CLK0 phase offset=0.
-    ( 0xA6, 0x00 ),  # CLK1 phase offset=0.
-    ( 0xA7, 0x00 ),  # CLK2 phase offset=0.
-    ( 0xA8, 0x00 ),  # CLK3 phase offset=0.
-    ( 0xA9, 0x00 ),  # CLK4 phase offset=0.
-    ( 0xAA, 0x00 ),  # CLK5 phase offset=0.
+    # Phase Offset Configuration
+    ( 0xA5, 0x00 ),
+    ( 0xA6, 0x00 ),
+    ( 0xA7, 0x00 ),
+    ( 0xA8, 0x00 ),
+    ( 0xA9, 0x00 ),
+    ( 0xAA, 0x00 ),
 
-    # Crystal Load Capacitance.
-    (0xB7, 0x12 ),  # XTAL_CL=0 (6pF loading).
+    # Crystal Load Capacitance
+    ( 0xB7, 0x12 ),  # XTAL_CL=0 (6pF loading)
 ]
 
 # LiteI2C Sequencer -----------------------------------------------------------------------------
