a8670483888924c49a48cd1b8ff6a5090a1a2c99
ismailessaidi11 <ismailessaidi11@gmail.com>
2025-11-27 22:56:17 +0100

    Issues found and fixed: 1. We were sending 2044 frames of 8 bytes but the header extractor expects to get 1024 frames at a time (misallignement of the data) 2. we were not sending the timestamp and the header.
    Attempt to solve the issues but still same result but I believe it is more consistent

--- flash_bitstream_SRAM.sh
+++ flash_bitstream_SRAM.sh
@@ -1,1 +1,3 @@
-openFPGALoader -c ch347_jtag ./build/litex_m2sdr_m2_pcie_x1/gateware/litex_m2sdr_m2_pcie_x1.bit
+# to flash the .bin  
+openFPGALoader -c ch347_jtag --freq 10e6 --fpga-part xc7a200t --write-flash ./build/litex_m2sdr_m2_pcie_x1/gateware/litex_m2sdr_m2_pcie_x1_operational.bin
+#openFPGALoader -c ch347_jtag ./build/litex_m2sdr_m2_pcie_x1/gateware/litex_m2sdr_m2_pcie_x1.bit
