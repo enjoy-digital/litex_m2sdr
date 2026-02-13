1110d5a66f920697dc07ab05da8bb0550ee74ba5
Florent Kermarrec <florent@enjoy-digital.fr>
2026-02-13 12:36:22 +0100

    software/user/m2sdr_sata: use shared get_time_ms helper to fix build

--- litex_m2sdr/software/user/m2sdr_sata.c
+++ litex_m2sdr/software/user/m2sdr_sata.c
@@ -66,13 +66,6 @@ static void msleep(unsigned ms)
     usleep(ms * 1000);
 }
 
-static int64_t get_time_ms(void)
-{
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return (int64_t)ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
-}
-
 /* Connection functions ------------------------------------------------------ */
 
 static void *m2sdr_open(void)
