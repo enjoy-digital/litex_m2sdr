07024ef531e5ae30ff3a3d279c89f5fe5789e387
Florent Kermarrec <florent@enjoy-digital.fr>
2026-03-12 09:23:58 +0100

    software/user: improve SigMF annotation navigation in m2sdr_check

--- litex_m2sdr/software/user/m2sdr_check.c
+++ litex_m2sdr/software/user/m2sdr_check.c
@@ -106,11 +106,14 @@ struct ui_state {
     int view_samples;
     int constellation_points;
     int fft_len_idx;
+    int selected_annotation;
     bool autoscale_time;
     bool autoscale_constellation;
     bool show_i;
     bool show_q;
     bool show_mag;
+    bool annotation_overlaps_only;
+    char annotation_filter[64];
 };
 
 static const int k_fft_lengths[] = {128, 256, 512, 1024, 2048, 4096, 8192, 16384};
@@ -762,26 +765,6 @@ static void draw_stats_panel(const struct check_data *data, int channel)
             igTextWrapped("%s", data->sigmf_meta.description);
         }
         igText("Annotations       : %u", data->sigmf_meta.annotation_count);
-        if (data->sigmf_meta.annotation_count > 0) {
-            unsigned i;
-            igText("Annotation list");
-            for (i = 0; i < data->sigmf_meta.annotation_count; i++) {
-                const struct m2sdr_sigmf_annotation *ann = &data->sigmf_meta.annotations[i];
-                igSeparator();
-                igText("#%u start=%" PRIu64, i, ann->sample_start);
-                if (ann->has_sample_count)
-                    igText("  count=%" PRIu64, ann->sample_count);
-                if (ann->label[0])
-                    igText("  label=%s", ann->label);
-                if (ann->has_freq_lower_edge || ann->has_freq_upper_edge) {
-                    double flo = ann->has_freq_lower_edge ? ann->freq_lower_edge / 1e6 : 0.0;
-                    double fhi = ann->has_freq_upper_edge ? ann->freq_upper_edge / 1e6 : 0.0;
-                    igText("  freq=%.6f..%.6f MHz", flo, fhi);
-                }
-                if (ann->comment[0])
-                    igTextWrapped("  %s", ann->comment);
-            }
-        }
     }
     if (data->frame_header) {
         igText("Headers seen      : %zu", data->headers_seen);
@@ -795,6 +778,122 @@ static void draw_stats_panel(const struct check_data *data, int channel)
         if (data->headers_seen > 2)
             igText("RMS ts jitter     : %.3f ns", data->rms_timestamp_jitter_ns);
     }
+}
+
+static int annotation_overlaps_view(const struct m2sdr_sigmf_annotation *ann, int start_sample, int view_samples)
+{
+    uint64_t view_start = (uint64_t)start_sample;
+    uint64_t view_end = (uint64_t)start_sample + (uint64_t)view_samples;
+    uint64_t ann_start = ann->sample_start;
+    uint64_t ann_end = ann->has_sample_count ? (ann->sample_start + ann->sample_count) : (ann->sample_start + 1);
+
+    return (ann_start < view_end) && (ann_end > view_start);
+}
+
+static int contains_case_insensitive(const char *haystack, const char *needle)
+{
+    size_t needle_len;
+    size_t i;
+
+    if (!needle || !needle[0])
+        return 1;
+    if (!haystack)
+        return 0;
+    needle_len = strlen(needle);
+    for (i = 0; haystack[i]; i++) {
+        size_t j = 0;
+        while (haystack[i + j] && needle[j] &&
+               tolower((unsigned char)haystack[i + j]) == tolower((unsigned char)needle[j])) {
+            j++;
+        }
+        if (j == needle_len)
+            return 1;
+    }
+    return 0;
+}
+
+static void draw_annotations_panel(const struct check_data *data, struct ui_state *ui)
+{
+    unsigned i;
+    unsigned visible = 0;
+    int selected_visible = 0;
+
+    if (!data->sigmf_loaded || data->sigmf_meta.annotation_count == 0)
+        return;
+
+    igSeparator();
+    igText("Annotations");
+    igCheckbox("Overlap only", &ui->annotation_overlaps_only);
+    igInputTextWithHint("##annotation_filter", "Filter label/comment", ui->annotation_filter,
+                        sizeof(ui->annotation_filter), 0, NULL, NULL);
+
+    if (ui->selected_annotation >= 0 && ui->selected_annotation < (int)data->sigmf_meta.annotation_count) {
+        const struct m2sdr_sigmf_annotation *ann = &data->sigmf_meta.annotations[ui->selected_annotation];
+        if (igButton("Jump to Selected", (ImVec2){0.0f, 0.0f})) {
+            ui->start_sample = (int)ann->sample_start;
+            if (ann->has_sample_count && ann->sample_count > 0 && ann->sample_count < (uint64_t)ui->view_samples) {
+                int centered = (int)ann->sample_start - (ui->view_samples - (int)ann->sample_count) / 2;
+                ui->start_sample = centered > 0 ? centered : 0;
+            }
+        }
+        igSameLine(0.0f, -1.0f);
+        if (igButton("Center on Selected", (ImVec2){0.0f, 0.0f})) {
+            int centered = (int)ann->sample_start - ui->view_samples / 2;
+            ui->start_sample = centered > 0 ? centered : 0;
+        }
+    }
+
+    if (!igBeginChild_Str("##annotation_list", (ImVec2){0.0f, 220.0f}, 1, 0)) {
+        igEndChild();
+        return;
+    }
+
+    for (i = 0; i < data->sigmf_meta.annotation_count; i++) {
+        const struct m2sdr_sigmf_annotation *ann = &data->sigmf_meta.annotations[i];
+        char label[256];
+        int overlaps = annotation_overlaps_view(ann, ui->start_sample, ui->view_samples);
+
+        if (ui->annotation_overlaps_only && !overlaps)
+            continue;
+        if (!contains_case_insensitive(ann->label, ui->annotation_filter) &&
+            !contains_case_insensitive(ann->comment, ui->annotation_filter))
+            continue;
+
+        visible++;
+        snprintf(label, sizeof(label), "#%u  @%" PRIu64 "%s%s",
+                 i, ann->sample_start,
+                 ann->label[0] ? "  " : "",
+                 ann->label[0] ? ann->label : "");
+        if (igSelectable_Bool(label, ui->selected_annotation == (int)i,
+                              ImGuiSelectableFlags_SpanAvailWidth, (ImVec2){0.0f, 0.0f})) {
+            ui->selected_annotation = (int)i;
+            selected_visible = 1;
+        }
+        if (overlaps) {
+            igSameLine(0.0f, -1.0f);
+            igTextDisabled("[in view]");
+        }
+        if (ui->selected_annotation == (int)i) {
+            selected_visible = 1;
+            if (ann->has_sample_count)
+                igText("  count=%" PRIu64, ann->sample_count);
+            if (ann->has_freq_lower_edge || ann->has_freq_upper_edge) {
+                double flo = ann->has_freq_lower_edge ? ann->freq_lower_edge / 1e6 : 0.0;
+                double fhi = ann->has_freq_upper_edge ? ann->freq_upper_edge / 1e6 : 0.0;
+                igText("  freq=%.6f..%.6f MHz", flo, fhi);
+            }
+            if (ann->comment[0])
+                igTextWrapped("  %s", ann->comment);
+            igSetScrollHereY(0.5f);
+        }
+    }
+
+    if (!selected_visible && ui->selected_annotation >= (int)data->sigmf_meta.annotation_count)
+        ui->selected_annotation = -1;
+    if (visible == 0)
+        igTextDisabled("No annotations match the current filter.");
+
+    igEndChild();
 }
 
 static void draw_time_domain(const struct ui_state *ui, const struct plot_cache *cache, int count)
@@ -897,11 +996,14 @@ int main(int argc, char **argv)
     ui.view_samples = CHECK_DEFAULT_VIEW_SAMPLES;
     ui.constellation_points = CHECK_DEFAULT_CONSTELLATION_POINTS;
     ui.fft_len_idx = 4;
+    ui.selected_annotation = -1;
     ui.autoscale_time = true;
     ui.autoscale_constellation = true;
     ui.show_i = true;
     ui.show_q = true;
     ui.show_mag = false;
+    ui.annotation_overlaps_only = false;
+    ui.annotation_filter[0] = '\0';
 
     for (;;) {
         c = getopt_long(argc, argv, "h", options, &option_index);
@@ -1106,6 +1208,7 @@ int main(int argc, char **argv)
                 draw_controls(&data, &ui);
                 igSeparator();
                 draw_stats_panel(&data, ui.selected_channel);
+                draw_annotations_panel(&data, &ui);
             }
             igEndChild();
 
