ccb4b2c6d977eccbb4308d9dac467fb03b4b5a6b
Florent Kermarrec <florent@enjoy-digital.fr>
2026-04-02 13:14:24 +0200

    gateware: add runtime PTP discipline controls
    Expose the Ethernet PTP discipline loop tuning knobs through CSRs and
    track lock-loss statistics.
    
    This makes the servo configurable at runtime and gives host software
    enough visibility to tune and diagnose the loop without rebuilding the
    bitstream.

--- litex_m2sdr/gateware/ptp_discipline.py
+++ litex_m2sdr/gateware/ptp_discipline.py
@@ -34,7 +34,7 @@ class TimeDisciplineCDC(LiteXModule):
             MultiReg(self.adjustment,  time_gen.discipline_adjustment,  "time"),
         ]
 
-        self.write_ps = PulseSynchronizer(cd_from, "time")
+        self.write_ps  = PulseSynchronizer(cd_from, "time")
         self.adjust_ps = PulseSynchronizer(cd_from, "time")
         self.submodules += self.write_ps, self.adjust_ps
         self.comb += [
@@ -49,13 +49,23 @@ class TimeDisciplineCDC(LiteXModule):
 class PTPTimeDiscipline(LiteXModule):
     """Discipline the SI5351-backed board time from LiteEth PTP time."""
 
-    STATE_MANUAL  = 0
-    STATE_ACQUIRE = 1
-    STATE_LOCKED  = 2
+    STATE_MANUAL   = 0
+    STATE_ACQUIRE  = 1
+    STATE_LOCKED   = 2
     STATE_HOLDOVER = 3
 
     def __init__(self, sys_clk_freq, nominal_time_inc, with_csr=True):
-        self.enable          = Signal(reset=1)
+        self.enable              = Signal(reset=1)
+        self.holdover_enable     = Signal(reset=1)
+        self.update_cycles_cfg   = Signal(32, reset=max(1, int(sys_clk_freq // 64)))
+        self.coarse_threshold_cfg = Signal(32, reset=100_000)
+        self.phase_threshold_cfg  = Signal(32, reset=128)
+        self.phase_lock_window_cfg = Signal(32, reset=500)
+        self.phase_step_shift_cfg = Signal(6,  reset=2)
+        self.phase_step_max_cfg   = Signal(32, reset=4_096)
+        self.trim_limit_cfg       = Signal(32, reset=200_000)
+        self.p_gain_cfg           = Signal(16, reset=8)
+
         self.local_time      = Signal(64)
         self.ptp_seconds     = Signal(48)
         self.ptp_nanoseconds = Signal(32)
@@ -69,67 +79,84 @@ class PTPTimeDiscipline(LiteXModule):
         self.discipline_adjust_sign = Signal()
         self.discipline_adjustment  = Signal(64)
 
-        self.active        = Signal()
-        self.locked        = Signal()
-        self.holdover      = Signal()
-        self.state         = Signal(2, reset=self.STATE_MANUAL)
-        self.last_error    = Signal((65, True))
-        self.last_ptp_time = Signal(64)
+        self.active          = Signal()
+        self.locked          = Signal()
+        self.holdover        = Signal()
+        self.state           = Signal(2, reset=self.STATE_MANUAL)
+        self.last_error      = Signal((65, True))
+        self.last_ptp_time   = Signal(64)
         self.last_local_time = Signal(64)
-        self.coarse_steps  = Signal(32)
-        self.phase_steps   = Signal(32)
-        self.rate_updates  = Signal(32)
+        self.coarse_steps    = Signal(32)
+        self.phase_steps     = Signal(32)
+        self.rate_updates    = Signal(32)
+        self.ptp_lock_losses = Signal(32)
+        self.time_lock_losses = Signal(32)
+        self.clear_counters  = Signal()
 
         # # #
 
-        update_cycles      = max(1, int(sys_clk_freq // 64))
-        coarse_threshold   = 100_000
-        phase_threshold    = 128
-        phase_lock_window  = 500
-        phase_step_max     = 4_096
-        trim_limit         = 200_000
-        p_gain             = 8
-        seconds_to_ns      = 1_000_000_000
-
-        sample_counter     = Signal(max=max(update_cycles + 1, 2))
-        sample_tick        = Signal()
-        ever_locked        = Signal()
-        integral_trim      = Signal((32, True))
-        ptp_time_ns        = Signal(64)
-        ptp_time_signed    = Signal((65, True))
-        local_time_signed  = Signal((65, True))
-        error_ns           = Signal((65, True))
-        error_abs          = Signal(65)
-        phase_step         = Signal(64)
-        p_term             = Signal((65, True))
-        trim_sum           = Signal((66, True))
-        trim_term          = Signal((32, True))
+        seconds_to_ns         = 1_000_000_000
+        sample_counter        = Signal(32)
+        sample_tick           = Signal()
+        ever_locked           = Signal()
+        prev_ptp_locked       = Signal()
+        prev_time_locked      = Signal()
+        integral_trim         = Signal((32, True))
+        ptp_time_ns           = Signal(64)
+        ptp_time_signed       = Signal((65, True))
+        local_time_signed     = Signal((65, True))
+        error_ns              = Signal((65, True))
+        error_abs             = Signal(65)
+        phase_shifted         = Signal(65)
+        phase_step            = Signal(64)
+        p_term               = Signal((81, True))
+        trim_sum             = Signal((82, True))
+        trim_term            = Signal((32, True))
+        effective_update_cycles = Signal(32)
+        next_locked          = Signal()
+        holdover_allowed     = Signal()
 
         self.comb += [
             self.discipline_enable.eq(self.enable),
             self.active.eq(self.enable),
+            holdover_allowed.eq(self.holdover_enable & ever_locked),
+            If(self.update_cycles_cfg == 0,
+                effective_update_cycles.eq(1),
+            ).Else(
+                effective_update_cycles.eq(self.update_cycles_cfg),
+            ),
             ptp_time_ns.eq((self.ptp_seconds[:34] * seconds_to_ns) + self.ptp_nanoseconds),
             ptp_time_signed.eq(ptp_time_ns),
             local_time_signed.eq(self.local_time),
             error_ns.eq(ptp_time_signed - local_time_signed),
             error_abs.eq(Mux(error_ns < 0, -error_ns, error_ns)),
-            p_term.eq(error_ns * p_gain),
+            phase_shifted.eq(error_abs >> self.phase_step_shift_cfg),
+            p_term.eq(error_ns * self.p_gain_cfg),
             trim_sum.eq(integral_trim + p_term),
             If(error_abs == 0,
                 phase_step.eq(0),
-            ).Elif((error_abs >> 2) == 0,
+            ).Elif(phase_shifted == 0,
                 phase_step.eq(1),
-            ).Elif((error_abs >> 2) > phase_step_max,
-                phase_step.eq(phase_step_max),
+            ).Elif(phase_shifted > self.phase_step_max_cfg,
+                phase_step.eq(self.phase_step_max_cfg),
             ).Else(
-                phase_step.eq(error_abs >> 2),
+                phase_step.eq(phase_shifted),
+            ),
+            If(~self.enable,
+                next_locked.eq(0),
+            ).Elif(~self.ptp_locked,
+                next_locked.eq(0),
+            ).Elif((error_abs >= self.coarse_threshold_cfg) | ~ever_locked,
+                next_locked.eq(0),
+            ).Else(
+                next_locked.eq(error_abs <= self.phase_lock_window_cfg),
             ),
         ]
 
         self.sync += [
             self.discipline_write.eq(0),
             self.discipline_adjust.eq(0),
-            If(sample_counter == (update_cycles - 1),
+            If(sample_counter >= (effective_update_cycles - 1),
                 sample_counter.eq(0),
                 sample_tick.eq(1),
             ).Else(
@@ -139,7 +166,13 @@ class PTPTimeDiscipline(LiteXModule):
         ]
 
         self.sync += [
-            If(~self.enable,
+            If(self.clear_counters,
+                self.coarse_steps.eq(0),
+                self.phase_steps.eq(0),
+                self.rate_updates.eq(0),
+                self.ptp_lock_losses.eq(0),
+                self.time_lock_losses.eq(0),
+            ).Elif(~self.enable,
                 self.discipline_time_inc.eq(nominal_time_inc),
                 self.discipline_write_time.eq(0),
                 self.discipline_adjust_sign.eq(0),
@@ -148,17 +181,27 @@ class PTPTimeDiscipline(LiteXModule):
                 self.holdover.eq(0),
                 self.state.eq(self.STATE_MANUAL),
                 ever_locked.eq(0),
+                prev_ptp_locked.eq(0),
+                prev_time_locked.eq(0),
                 integral_trim.eq(0),
                 self.last_error.eq(0),
                 self.last_ptp_time.eq(0),
                 self.last_local_time.eq(0),
             ).Elif(sample_tick,
+                If(prev_ptp_locked & ~self.ptp_locked,
+                    self.ptp_lock_losses.eq(self.ptp_lock_losses + 1),
+                ),
+                If(prev_time_locked & ~next_locked,
+                    self.time_lock_losses.eq(self.time_lock_losses + 1),
+                ),
+                prev_ptp_locked.eq(self.ptp_locked),
+                prev_time_locked.eq(next_locked),
                 self.last_error.eq(error_ns),
                 self.last_ptp_time.eq(ptp_time_ns),
                 self.last_local_time.eq(self.local_time),
                 If(self.ptp_locked,
                     self.holdover.eq(0),
-                    If((error_abs >= coarse_threshold) | ~ever_locked,
+                    If((error_abs >= self.coarse_threshold_cfg) | ~ever_locked,
                         self.discipline_write.eq(1),
                         self.discipline_write_time.eq(ptp_time_ns),
                         self.discipline_time_inc.eq(nominal_time_inc),
@@ -170,40 +213,44 @@ class PTPTimeDiscipline(LiteXModule):
                         ever_locked.eq(1),
                         integral_trim.eq(0),
                     ).Else(
-                        If(error_abs >= phase_threshold,
+                        If(error_abs >= self.phase_threshold_cfg,
                             self.discipline_adjust.eq(1),
                             self.discipline_adjust_sign.eq(error_ns < 0),
                             self.discipline_adjustment.eq(phase_step),
                             self.phase_steps.eq(self.phase_steps + 1),
                         ),
-                        If(trim_sum < -trim_limit,
-                            trim_term.eq(-trim_limit),
-                        ).Elif(trim_sum > trim_limit,
-                            trim_term.eq(trim_limit),
+                        If(trim_sum < -self.trim_limit_cfg,
+                            trim_term.eq(-self.trim_limit_cfg),
+                        ).Elif(trim_sum > self.trim_limit_cfg,
+                            trim_term.eq(self.trim_limit_cfg),
                         ).Else(
                             trim_term.eq(trim_sum),
                         ),
-                        If((integral_trim + error_ns) < -trim_limit,
-                            integral_trim.eq(-trim_limit),
-                        ).Elif((integral_trim + error_ns) > trim_limit,
-                            integral_trim.eq(trim_limit),
+                        If((integral_trim + error_ns) < -self.trim_limit_cfg,
+                            integral_trim.eq(-self.trim_limit_cfg),
+                        ).Elif((integral_trim + error_ns) > self.trim_limit_cfg,
+                            integral_trim.eq(self.trim_limit_cfg),
                         ).Else(
                             integral_trim.eq(integral_trim + error_ns),
                         ),
                         self.discipline_time_inc.eq(nominal_time_inc + trim_term),
                         self.rate_updates.eq(self.rate_updates + 1),
-                        self.locked.eq(error_abs <= phase_lock_window),
-                        self.state.eq(Mux(error_abs <= phase_lock_window, self.STATE_LOCKED, self.STATE_ACQUIRE)),
+                        self.locked.eq(next_locked),
+                        self.state.eq(Mux(next_locked, self.STATE_LOCKED, self.STATE_ACQUIRE)),
                         ever_locked.eq(1),
                     )
                 ).Else(
                     self.locked.eq(0),
-                    If(ever_locked,
+                    If(holdover_allowed,
                         self.holdover.eq(1),
                         self.state.eq(self.STATE_HOLDOVER),
                     ).Else(
                         self.holdover.eq(0),
                         self.state.eq(self.STATE_ACQUIRE),
+                        self.discipline_time_inc.eq(nominal_time_inc),
+                        self.discipline_adjust_sign.eq(0),
+                        self.discipline_adjustment.eq(0),
+                        integral_trim.eq(0),
                     ),
                 )
             )
@@ -218,25 +265,51 @@ class PTPTimeDiscipline(LiteXModule):
                 ("``0b0``", "Disable PTP ownership of the board time generator."),
                 ("``0b1``", "Discipline the board time generator from Ethernet PTP."),
             ]),
+            CSRField("holdover", size=1, offset=1, reset=1, values=[
+                ("``0b0``", "Drop back to acquire mode when PTP lock is lost."),
+                ("``0b1``", "Keep the last disciplined rate while waiting for PTP lock to return."),
+            ]),
         ])
-        self._status = CSRStatus(fields=[
-            CSRField("enable",     size=1, offset=0, description="PTP time discipline enabled."),
-            CSRField("active",     size=1, offset=1, description="PTP discipline currently owns the board time generator."),
-            CSRField("ptp_locked", size=1, offset=2, description="LiteEth PTP core is locked to a master."),
-            CSRField("time_locked",size=1, offset=3, description="Board time is within the lock window of PTP time."),
-            CSRField("holdover",   size=1, offset=4, description="PTP lock was lost; keeping the last disciplined rate."),
-            CSRField("state",      size=2, offset=8, description="0=manual, 1=acquire, 2=locked, 3=holdover."),
-        ])
-        self._time_inc       = CSRStatus(32, description="Disciplined TimeGenerator increment (Q8.24 ns/tick).")
-        self._last_error     = CSRStatus(64, description="Last signed PTP minus board-time error (ns, two's complement).")
-        self._last_ptp_time  = CSRStatus(64, description="Last sampled PTP time (ns).")
-        self._last_local_time = CSRStatus(64, description="Last sampled board time (ns).")
-        self._coarse_steps   = CSRStatus(32, description="Number of coarse board-time realignments.")
-        self._phase_steps    = CSRStatus(32, description="Number of bounded phase trims.")
-        self._rate_updates   = CSRStatus(32, description="Number of rate trim updates.")
+        self._clear_counters = CSRStorage(description="Write to clear the PTP discipline counters.")
+        self._update_cycles = CSRStorage(32, reset=self.update_cycles_cfg.reset.value, description="Servo update interval in sys_clk cycles.")
+        self._coarse_threshold = CSRStorage(32, reset=self.coarse_threshold_cfg.reset.value, description="Coarse realignment threshold in nanoseconds.")
+        self._phase_threshold = CSRStorage(32, reset=self.phase_threshold_cfg.reset.value, description="Bounded phase-trim threshold in nanoseconds.")
+        self._lock_window = CSRStorage(32, reset=self.phase_lock_window_cfg.reset.value, description="Time-lock window in nanoseconds.")
+        self._phase_step_shift = CSRStorage(6, reset=self.phase_step_shift_cfg.reset.value, description="Right shift applied to the absolute phase error before bounded trims.")
+        self._phase_step_max = CSRStorage(32, reset=self.phase_step_max_cfg.reset.value, description="Maximum bounded phase trim in nanoseconds.")
+        self._trim_limit = CSRStorage(32, reset=self.trim_limit_cfg.reset.value, description="Maximum frequency trim in time_inc LSB units.")
+        self._p_gain = CSRStorage(16, reset=self.p_gain_cfg.reset.value, description="Proportional gain applied to the phase error before rate trim limiting.")
 
-        self.specials += MultiReg(self._control.fields.enable, self.enable)
+        self._status = CSRStatus(fields=[
+            CSRField("enable",      size=1, offset=0, description="PTP time discipline enabled."),
+            CSRField("active",      size=1, offset=1, description="PTP discipline currently owns the board time generator."),
+            CSRField("ptp_locked",  size=1, offset=2, description="LiteEth PTP core is locked to a master."),
+            CSRField("time_locked", size=1, offset=3, description="Board time is within the lock window of PTP time."),
+            CSRField("holdover",    size=1, offset=4, description="PTP lock was lost; keeping the last disciplined rate."),
+            CSRField("state",       size=2, offset=8, description="0=manual, 1=acquire, 2=locked, 3=holdover."),
+        ])
+        self._time_inc = CSRStatus(32, description="Disciplined TimeGenerator increment (Q8.24 ns/tick).")
+        self._last_error = CSRStatus(64, description="Last signed PTP minus board-time error (ns, two's complement).")
+        self._last_ptp_time = CSRStatus(64, description="Last sampled PTP time (ns).")
+        self._last_local_time = CSRStatus(64, description="Last sampled board time (ns).")
+        self._coarse_steps = CSRStatus(32, description="Number of coarse board-time realignments.")
+        self._phase_steps = CSRStatus(32, description="Number of bounded phase trims.")
+        self._rate_updates = CSRStatus(32, description="Number of rate trim updates.")
+        self._ptp_lock_losses = CSRStatus(32, description="Number of PTP lock-loss events observed by the discipline loop.")
+        self._time_lock_losses = CSRStatus(32, description="Number of board-time lock-loss events observed by the discipline loop.")
+
         self.comb += [
+            self.enable.eq(self._control.fields.enable),
+            self.holdover_enable.eq(self._control.fields.holdover),
+            self.clear_counters.eq(self._clear_counters.re),
+            self.update_cycles_cfg.eq(self._update_cycles.storage),
+            self.coarse_threshold_cfg.eq(self._coarse_threshold.storage),
+            self.phase_threshold_cfg.eq(self._phase_threshold.storage),
+            self.phase_lock_window_cfg.eq(self._lock_window.storage),
+            self.phase_step_shift_cfg.eq(self._phase_step_shift.storage),
+            self.phase_step_max_cfg.eq(self._phase_step_max.storage),
+            self.trim_limit_cfg.eq(self._trim_limit.storage),
+            self.p_gain_cfg.eq(self._p_gain.storage),
             self._status.fields.enable.eq(self.enable),
             self._status.fields.active.eq(self.active),
             self._status.fields.ptp_locked.eq(self.ptp_locked),
@@ -250,4 +323,6 @@ class PTPTimeDiscipline(LiteXModule):
             self._coarse_steps.status.eq(self.coarse_steps),
             self._phase_steps.status.eq(self.phase_steps),
             self._rate_updates.status.eq(self.rate_updates),
+            self._ptp_lock_losses.status.eq(self.ptp_lock_losses),
+            self._time_lock_losses.status.eq(self.time_lock_losses),
         ]
--- test/test_ptp_discipline.py
+++ test/test_ptp_discipline.py
@@ -18,6 +18,7 @@ def test_ptp_discipline_requests_coarse_step_on_first_lock():
 
     def gen():
         yield dut.enable.eq(1)
+        yield dut.update_cycles_cfg.eq(1)
         yield dut.local_time.eq(0)
         yield dut.ptp_seconds.eq(1)
         yield dut.ptp_nanoseconds.eq(234)
@@ -39,14 +40,13 @@ def test_ptp_discipline_enters_holdover_and_keeps_last_trim():
 
     def gen():
         yield dut.enable.eq(1)
+        yield dut.update_cycles_cfg.eq(1)
 
-        # First cycle: acquire from a large error.
         yield dut.local_time.eq(0)
         yield dut.ptp_nanoseconds.eq(500_000)
         yield dut.ptp_locked.eq(1)
         yield
 
-        # Next sample: near-lock with a small positive error.
         yield dut.local_time.eq(100_100)
         yield dut.ptp_nanoseconds.eq(100_000)
         yield dut.ptp_locked.eq(1)
@@ -54,7 +54,6 @@ def test_ptp_discipline_enters_holdover_and_keeps_last_trim():
             yield
         seen["time_inc_locked"] = (yield dut.discipline_time_inc)
 
-        # Lose PTP lock and ensure holdover is asserted while keeping the last trim.
         yield dut.ptp_locked.eq(0)
         for _ in range(3):
             yield
@@ -70,3 +69,98 @@ def test_ptp_discipline_enters_holdover_and_keeps_last_trim():
     assert seen["state"] == dut.STATE_HOLDOVER
     assert seen["time_inc_holdover"] != (10 << 24)
     assert seen["time_inc_holdover_stable"] == seen["time_inc_holdover"]
+
+
+def test_ptp_discipline_holdover_can_be_disabled_at_runtime():
+    dut = PTPTimeDiscipline(sys_clk_freq=64, nominal_time_inc=(10 << 24), with_csr=False)
+    seen = {}
+
+    def gen():
+        yield dut.enable.eq(1)
+        yield dut.holdover_enable.eq(0)
+        yield dut.update_cycles_cfg.eq(1)
+
+        yield dut.local_time.eq(0)
+        yield dut.ptp_nanoseconds.eq(250_000)
+        yield dut.ptp_locked.eq(1)
+        yield
+
+        yield dut.local_time.eq(100_050)
+        yield dut.ptp_nanoseconds.eq(100_000)
+        yield dut.ptp_locked.eq(1)
+        for _ in range(3):
+            yield
+        seen["time_inc_locked"] = (yield dut.discipline_time_inc)
+
+        yield dut.ptp_locked.eq(0)
+        for _ in range(3):
+            yield
+        seen["holdover"] = (yield dut.holdover)
+        seen["state"] = (yield dut.state)
+        seen["time_inc_after_unlock"] = (yield dut.discipline_time_inc)
+
+    run_simulation(dut, gen())
+    assert seen["time_inc_locked"] != (10 << 24)
+    assert seen["holdover"] == 0
+    assert seen["state"] == dut.STATE_ACQUIRE
+    assert seen["time_inc_after_unlock"] == (10 << 24)
+
+
+def test_ptp_discipline_counts_lock_losses_after_runtime_lock():
+    dut = PTPTimeDiscipline(sys_clk_freq=64, nominal_time_inc=(10 << 24), with_csr=False)
+    seen = {}
+
+    def gen():
+        yield dut.enable.eq(1)
+        yield dut.update_cycles_cfg.eq(1)
+        yield dut.coarse_threshold_cfg.eq(1_000_000)
+        yield dut.phase_lock_window_cfg.eq(64)
+
+        yield dut.local_time.eq(0)
+        yield dut.ptp_nanoseconds.eq(100_000)
+        yield dut.ptp_locked.eq(1)
+        yield
+
+        yield dut.local_time.eq(100_000)
+        yield dut.ptp_nanoseconds.eq(100_000)
+        yield dut.ptp_locked.eq(1)
+        for _ in range(3):
+            yield
+        seen["time_locked"] = (yield dut.locked)
+
+        yield dut.ptp_locked.eq(0)
+        for _ in range(3):
+            yield
+        seen["ptp_lock_losses"] = (yield dut.ptp_lock_losses)
+        seen["time_lock_losses"] = (yield dut.time_lock_losses)
+
+    run_simulation(dut, gen())
+    assert seen["time_locked"] == 1
+    assert seen["ptp_lock_losses"] == 1
+    assert seen["time_lock_losses"] == 1
+
+
+def test_ptp_discipline_runtime_phase_threshold_suppresses_small_adjustments():
+    dut = PTPTimeDiscipline(sys_clk_freq=64, nominal_time_inc=(10 << 24), with_csr=False)
+    seen = {"adjust": 0}
+
+    def gen():
+        yield dut.enable.eq(1)
+        yield dut.update_cycles_cfg.eq(1)
+        yield dut.phase_threshold_cfg.eq(512)
+
+        yield dut.local_time.eq(0)
+        yield dut.ptp_nanoseconds.eq(2_000)
+        yield dut.ptp_locked.eq(1)
+        yield
+
+        yield dut.local_time.eq(1_950)
+        yield dut.ptp_nanoseconds.eq(2_000)
+        yield dut.ptp_locked.eq(1)
+        for _ in range(4):
+            if (yield dut.discipline_adjust):
+                seen["adjust"] = 1
+            yield
+
+    run_simulation(dut, gen())
+    assert seen["adjust"] == 0
