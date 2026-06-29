enjoy-digital/litex_m2sdr

commit 270f3cf8403123cf0a5f74e351fe497e110fa7d8

software/kernel/main: Fix/Simplify litepcie_mmap using linux/dma-mapping.h.
Thanks @fnclovers, see https://github.com/enjoy-digital/litex_m2sdr/issues/77.

litex_m2sdr/software/kernel/main.c
@@ -33,10 +33,7 @@
 #include <linux/cdev.h>
 #include <linux/platform_device.h>
 #include <linux/version.h>
-
-#if defined(__arm__) || defined(__aarch64__)
-#include <linux/dma-direct.h>
-#endif
+#include <linux/dma-mapping.h>
 
 #include <linux/ptp_clock_kernel.h>
 
@@ -618,8 +615,7 @@ static int litepcie_mmap(struct file *file, struct vm_area_struct *vma)
 	struct litepcie_chan_priv *chan_priv = file->private_data;
 	struct litepcie_chan *chan = chan_priv->chan;
 	struct litepcie_device *s = chan->litepcie_dev;
-	unsigned long pfn;
-	int is_tx, i;
+	int is_tx, i, ret;
 
 	if (vma->vm_end - vma->vm_start != DMA_BUFFER_TOTAL_SIZE)
 		return -EINVAL;
@@ -632,31 +628,28 @@ static int litepcie_mmap(struct file *file, struct vm_area_struct *vma)
 		return -EINVAL;
 
 	for (i = 0; i < DMA_BUFFER_COUNT; i++) {
-#if defined(__arm__) || defined(__aarch64__)
-		void *va;
-		if (i == 0)
-			dev_info(&s->dev->dev, "Using ARM/AArch64 DMA buffer handling");
-		if (is_tx)
-			va = phys_to_virt(dma_to_phys(&s->dev->dev, chan->dma.reader_handle[i]));
-		else
-			va = phys_to_virt(dma_to_phys(&s->dev->dev, chan->dma.writer_handle[i]));
-		pfn = page_to_pfn(virt_to_page(va));
-#else
-		if (i == 0)
-			dev_info(&s->dev->dev, "Using non-ARM DMA buffer handling");
-		if (is_tx)
-			pfn = __pa(chan->dma.reader_addr[i]) >> PAGE_SHIFT;
-		else
-			pfn = __pa(chan->dma.writer_addr[i]) >> PAGE_SHIFT;
-#endif
-		/*
-		 * Note: the memory is cached, so the user must explicitly
-		 * flush the CPU caches on architectures which require it.
-		 */
-		if (remap_pfn_range(vma, vma->vm_start + i * DMA_BUFFER_SIZE, pfn,
-				    DMA_BUFFER_SIZE, vma->vm_page_prot)) {
-			dev_err(&s->dev->dev, "mmap remap_pfn_range failed\n");
-			return -EAGAIN;
+		void *cpu_addr;
+		dma_addr_t dma_handle;
+		struct vm_area_struct sub_vma = *vma; /* map one chunk at a time */
+
+		if (is_tx) {
+			cpu_addr   = chan->dma.reader_addr[i];
+			dma_handle = chan->dma.reader_handle[i];
+		} else {
+			cpu_addr   = chan->dma.writer_addr[i];
+			dma_handle = chan->dma.writer_handle[i];
+		}
+
+		sub_vma.vm_start = vma->vm_start + (i * DMA_BUFFER_SIZE);
+		sub_vma.vm_end   = sub_vma.vm_start + DMA_BUFFER_SIZE;
+		sub_vma.vm_pgoff = 0; /* required: offset inside each coherent buffer */
+
+		ret = dma_mmap_coherent(&s->dev->dev, &sub_vma,
+					cpu_addr, dma_handle, DMA_BUFFER_SIZE);
+		if (ret) {
+			dev_err(&s->dev->dev,
+				"dma_mmap_coherent failed for buffer %d (ret=%d)\n", i, ret);
+			return ret;
 		}
 	}
 

commit 8dd2a02285fd56df4f41ca35bfc796d6b100672c

kernel: map zero-copy DMA pages through real VMA
The non-ARM zero-copy mmap path mapped each coherent DMA buffer by passing a copied sub-VMA to dma_mmap_coherent(). This installed PTEs in the process mm, but kept the VM_IO/VM_PFNMAP metadata on the temporary VMA, which newer kernels report as Bad page map during munmap().

Map each page of the coherent DMA buffers directly into the real VMA instead. vmalloc-backed coherent allocations, as used on IOMMU systems, are handled with vmalloc_to_pfn(), while direct allocations continue to use virt_to_page().

Fixes: 270f3cf84031 (software/kernel/main: Fix/Simplify litepcie_mmap using linux/dma-mapping.h.)

Closes: https://github.com/enjoy-digital/litex_m2sdr/issues/85

litex_m2sdr/software/kernel/main.c
@@ -39,6 +39,7 @@
 #include <linux/dma-mapping.h>
 #include <linux/device.h>
 #include <linux/uaccess.h>
+#include <linux/vmalloc.h>
 #if defined(__arm__) || defined(__aarch64__)
 #include <linux/dma-direct.h>
 #endif
@@ -1003,6 +1004,39 @@ static ssize_t litepcie_write(struct file *file, const char __user *data, size_t
 	return size - len;
 }
 
+/* Map the per-buffer coherent allocations into one userspace VMA without
+ * using copied sub-VMAs: the PTEs must be installed with PFNMAP metadata on
+ * the real VMA so munmap() can tear the mapping down cleanly.
+ */
+static unsigned long litepcie_dma_buffer_pfn(void *addr)
+{
+	if (is_vmalloc_addr(addr))
+		return vmalloc_to_pfn(addr);
+
+	return page_to_pfn(virt_to_page(addr));
+}
+
+static int litepcie_dma_buffer_mmap(struct device *dev, struct vm_area_struct *vma,
+				    unsigned long user_addr, void *cpu_addr)
+{
+	unsigned long offset;
+
+	if (DMA_BUFFER_SIZE % PAGE_SIZE)
+		return -EINVAL;
+
+	for (offset = 0; offset < DMA_BUFFER_SIZE; offset += PAGE_SIZE) {
+		unsigned long pfn = litepcie_dma_buffer_pfn((u8 *)cpu_addr + offset);
+
+		if (remap_pfn_range(vma, user_addr + offset, pfn, PAGE_SIZE,
+				    vma->vm_page_prot)) {
+			dev_err(dev, "mmap remap_pfn_range failed\n");
+			return -EAGAIN;
+		}
+	}
+
+	return 0;
+}
+
 /* Memory map DMA buffers for user space access */
 static int litepcie_mmap(struct file *file, struct vm_area_struct *vma)
 {
@@ -1047,31 +1081,20 @@ static int litepcie_mmap(struct file *file, struct vm_area_struct *vma)
 		}
 #else
 		void *cpu_addr;
-		dma_addr_t dma_handle;
-		struct vm_area_struct sub_vma = *vma; /* Map one chunk at a time */
 
 		if (i == 0)
 			dev_info(&s->dev->dev, "Using non-ARM DMA buffer handling");
 
-		if (is_tx) {
-			cpu_addr   = chan->dma.reader_addr[i];
-			dma_handle = chan->dma.reader_handle[i];
-		} else {
-			cpu_addr   = chan->dma.writer_addr[i];
-			dma_handle = chan->dma.writer_handle[i];
-		}
+		if (is_tx)
+			cpu_addr = chan->dma.reader_addr[i];
+		else
+			cpu_addr = chan->dma.writer_addr[i];
 
-		sub_vma.vm_start = vma->vm_start + (i * DMA_BUFFER_SIZE);
-		sub_vma.vm_end   = sub_vma.vm_start + DMA_BUFFER_SIZE;
-		sub_vma.vm_pgoff = 0; /* Required: offset inside each coherent buffer */
-
-		ret = dma_mmap_coherent(&s->dev->dev, &sub_vma,
-					cpu_addr, dma_handle, DMA_BUFFER_SIZE);
-		if (ret) {
-			dev_err(&s->dev->dev,
-				"dma_mmap_coherent failed for buffer %d (ret=%d)\n", i, ret);
+		ret = litepcie_dma_buffer_mmap(&s->dev->dev, vma,
+					       vma->vm_start + i * DMA_BUFFER_SIZE,
+					       cpu_addr);
+		if (ret)
 			return ret;
-		}
 #endif
 	}
 